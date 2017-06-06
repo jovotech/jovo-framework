93be71fb8b4aa80cc177b24b8dd426c9db993983
jovotech <a.swetlow@gmail.com>
2017-06-06 17:08:50 -0400

    eslint integrated (extend google config) first tests

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * Created by Alex on 02-Jun-17.
  */
 
-const Jovo = require("./lib/jovo").Jovo;
+const Jovo = require('./lib/jovo').Jovo;
 
 
-let express = require('express')
+let express = require('express');
 let bodyParser = require('body-parser');
 let server = express();
 server.use(bodyParser.json());
