a9b62cdf8158d1ed0fdf9514d91bb3b048d9377c
jovotech <a.swetlow@gmail.com>
2017-08-15 00:39:35 +0200

    :white_check_mark: Added random output tests

--- test/testSpeechBuilder.js
+++ test/testSpeechBuilder.js
@@ -99,4 +99,42 @@ describe('SpeechBuilder Class', function() {
             assert.ok(speech.build() === 'Hey Your name?<audio src="https://any.url.com/file.mp3"/>');
         });
     });
+
+    describe('array as parameter. ', function() {
+        it('should return one of the three values', function() {
+            let speech = (new Jovo.SpeechBuilder(Jovo.PLATFORM_ENUM.ALEXA_SKILL))
+                .addText(['Hey', 'Hi', 'Hello']);
+            assert.ok(speech.build() === 'Hey' ||
+                speech.build() === 'Hi' ||
+                speech.build() === 'Hello');
+
+            let speech2 = (new Jovo.SpeechBuilder(Jovo.PLATFORM_ENUM.ALEXA_SKILL))
+                .addBreak(['100ms', '1s', '500ms']);
+            assert.ok(speech2.build() === '<break time="100ms"/>' ||
+                speech2.build() === '<break time="1s"/>' ||
+                speech2.build() === '<break time="500ms"/>');
+
+            let speech3 = (new Jovo.SpeechBuilder(Jovo.PLATFORM_ENUM.ALEXA_SKILL))
+                .addAudio(['url1', 'url2', 'url3']);
+            assert.ok(speech3.build() === '<audio src="url1"/>' ||
+                speech3.build() === '<audio src="url2"/>' ||
+                speech3.build() === '<audio src="url3"/>');
+
+            let speech4 = (new Jovo.SpeechBuilder(Jovo.PLATFORM_ENUM.GOOGLE_ACTION))
+                .addAudio(
+                    [
+                        'url1',
+                        'url2',
+                        'url3',
+                    ],
+                    [
+                        'text1',
+                        'text2',
+                        'text3',
+                    ]);
+            assert.ok(speech4.build() === '<audio src="url1">text1</audio>' ||
+                speech4.build() === '<audio src="url2">text2</audio>' ||
+                speech4.build() === '<audio src="url3">text3</audio>');
+        });
+    });
 });
