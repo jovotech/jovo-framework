9a722ec61f0b182b549f4207b2120ee5ad9005f8
jovotech <a.swetlow@gmail.com>
2017-08-25 13:45:37 +0200

    :memo: Added example files

---
+++ examples/indexAlexaDeviceAddress.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const webhook = require('../index').Webhook;
+
+webhook.listen(3000, function() {
+    console.log('Example server listening on port 3000!');
+});
+
+const app = require('../index').Jovo;
+app.enableRequestLogging();
+app.enableResponseLogging();
+
+// listen for post requests
+webhook.post('/webhook', function(req, res) {
+    app.handleRequest(req, res, handlers);
+    app.execute();
+});
+
+
+let handlers = {
+
+    'LAUNCH': function() {
+       // app.toIntent('GetFullAddressIntent');
+       app.toIntent('GetCountryPostalCodeIntent');
+    },
+    'GetFullAddressIntent': function() {
+        app.user().getAddress()
+            .then((data) => {
+                console.log(data);
+                app.tell('Your address');
+            }).catch((error) => {
+            if (error.code === 'NO_USER_PERMISSION') {
+                app
+                    .showAskForAddressCard()
+                    .tell('Please grant access to your address');
+            }
+        });
+    },
+    'GetCountryPostalCodeIntent': function() {
+        app.user().getCountryAndPostalCode()
+            .then((data) => {
+                console.log(data);
+                app.tell('Your address');
+            }).catch((error) => {
+            console.log(error);
+            if (error.code === 'NO_USER_PERMISSION') {
+                app
+                    .showAskForCountryAndPostalCodeCard()
+                    .tell('Please grant access to your address');
+            }
+        });
+    },
+};
---
+++ examples/indexAlexaLists.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const webhook = require('../index').Webhook;
+
+webhook.listen(3000, function() {
+    console.log('Example server listening on port 3000!');
+});
+
+const app = require('../index').Jovo;
+app.enableRequestLogging();
+// app.enableResponseLogging();
+
+// listen for post requests
+webhook.post('/webhook', function(req, res) {
+    app.handleRequest(req, res, handlers);
+    app.execute();
+});
+
+
+let handlers = {
+
+    'LAUNCH': function() {
+       // app.toIntent('GetShoppingListIntent');
+       //  app.toIntent('GetTodoListIntent');
+       // app.toIntent('AddItemToToDoListIntent');
+       app.toIntent('UpdateItemIntent');
+    },
+    'GetShoppingListIntent': function() {
+        // active or completed
+        app.user().getShoppingList('active')
+            .then((data) => {
+                // iterate items on list
+                for (let obj of data.items) {
+                    app.speech.addSentence(obj.value);
+                }
+                app.tell(app.speech);
+            })
+            .catch((error) => {
+                if (error.code === 'NO_USER_PERMISSION') {
+                    app
+                        .showAskForListPermissionCard(['read'])
+                        .tell('Please grant the permission to access your lists.');
+                }
+            });
+    },
+    'GetTodoListIntent': function() {
+        // active or completed
+        app.user().getToDoList('active')
+            .then((data) => {
+                // iterate items on list
+                for (let obj of data.items) {
+                    app.speech.addSentence(obj.value);
+                }
+                app.tell(app.speech);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    'UpdateItemIntent': function() {
+        app.user().updateToDoList('Pay bills', 'Go Shopping', 'active')
+            .then((data) => {
+            console.log(data);
+                app.tell('item changed');
+            })
+            .catch((error) => {
+                if (error.code === 'NO_USER_PERMISSION') {
+                    app
+                        .showAskForListPermissionCard(['read', 'write'])
+                        .tell('Please grant the permission to access your lists');
+                }
+                if (error.code === 'ITEM_NOT_FOUND') {
+                    app
+                        .tell('Item not found');
+                }
+        });
+    },
+    'AddItemToToDoListIntent': function() {
+        app.user().addToTodoList('Sleep')
+            .then((data) => {
+                app.tell('Item added');
+            })
+            .catch((error) => {
+                if (error.code === 'NO_USER_PERMISSION') {
+                    app
+                        .showAskForListPermissionCard(['read', 'write'])
+                        .tell('Please grant the permission to access your lists');
+                }
+                console.log(error);
+            });
+    },
+
+};
