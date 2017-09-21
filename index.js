ab948f2e516c083c0af0ec10b02822641490164b
Juan Perata <juan@bespoken.tools>
2017-09-21 16:43:14 -0500

    Remove typo

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ if (process.argv.length > 2) {
 module.exports.Webhook = server;
 module.exports.WebhookVerified = verifiedServer;
 module.exports.Jovo = new Jovo();
-module.exports.GoogleAction = require('./lib/platforms/googleaction/googleAction').GoogleAction; ;
+module.exports.GoogleAction = require('./lib/platforms/googleaction/googleAction').GoogleAction;
 module.exports.AlexaSkill = require('./lib/platforms/alexa/alexaSkill').AlexaSkill;
 
 module.exports.FilePersistence = FilePersistence;
