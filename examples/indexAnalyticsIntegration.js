db745186acf50e073ee02ba669ce7790e6cd56f1
jovotech <a.swetlow@gmail.com>
2017-12-13 11:16:21 +0100

    :hammer: Refactored stuff

--- examples/indexAnalyticsIntegration.js
+++ examples/indexAnalyticsIntegration.js
@@ -11,13 +11,23 @@ const app = require('../index').Jovo;
 app.setConfig({
     requestLogging: true,
     responseLogging: true,
+    analytics: {
+        services: {
+            VoiceLabsAlexa: {
+                key: '23b55e80-3e3f-11a7-2167-0e2486876586',
+            },
+            DashbotAlexa: {
+                key: '6Idg1n3pCbk4bMh4XYubQVx5ckjVtuHctMND4sMM',
+            },
+        },
+    },
 });
 
 // Add Analytics Integrations
 // app.addVoiceLabsAlexa('23b55e80-3e3f-11a7-2167-0e2486876586');
 // app.addVoiceLabsGoogleAction('228f1d00-5764-11a7-023a-0e2486876586');
-app.addDashbotGoogleAction('1T3ZonKcbNzLv6eFLBwI9Fv8JAU5SBVorCCvvD1D');
-app.addDashbotAlexa('6Idg1n3pCbk4bMh4XYubQVx5ckjVtuHctMND4sMM');
+// app.addDashbotGoogleAction('1T3ZonKcbNzLv6eFLBwI9Fv8JAU5SBVorCCvvD1D');
+// app.addDashbotAlexa('6Idg1n3pCbk4bMh4XYubQVx5ckjVtuHctMND4sMM');
 // app.addBespokenAnalytics('Bespoken secret key');
 // Listen for post requests
 webhook.listen(3000, function() {
@@ -37,6 +47,7 @@ webhook.post('/webhook', function(req, res) {
 let handlers = {
 
     'LAUNCH': function() {
+        app.hasVideoInterface()
         app.toIntent('HelloWorldIntent');
     },
 
