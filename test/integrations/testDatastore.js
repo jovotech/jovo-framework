41d814cea589c5c6c5284e98d35384de9951470c
Bastian Frank <git@bastian-frank.de>
2018-03-16 13:44:29 +0100

    removed tests cases as local simulator is necessary

--- test/integrations/testDatastore.js
+++ test/integrations/testDatastore.js
@@ -2,11 +2,10 @@
 * You have to run the local datastore simulator for this to work!
 ***************************************/
 
-var assert = require('chai').assert;
-var request = require('request');
+/*var assert = require('chai').assert;
 var uuid = require('uuid/v4');
-var DatastoreDb = require('../app/patches/datastore.js').DatastoreDb;
-
+var DatastoreDb = require('../../lib/integrations/db/datastore.js').DatastoreDb;
+*/
 
 /**
 * Some settings
@@ -18,12 +17,12 @@ var config = {
 
 describe('Datastore jovo Connector', function() {
 
-  var originalValue = uuid();
+/*  var originalValue = uuid();
   var datastore = new DatastoreDb(config);
   var filestore = new FilePersistance("./test_db.json");
   datastore.setMainKey("user1");
-
-  it('should save a value', function(done) {
+*/
+  it.skip('should save a value', function(done) {
       datastore.save("test", originalValue, function(err, data) {
         assert.equal(err, undefined);
         done();
@@ -31,14 +30,14 @@ describe('Datastore jovo Connector', function() {
 
   });
 
-  it('should load the same value', function(done) {
+  it.skip('should load the same value', function(done) {
       datastore.load("test", function(err, v) {
         assert.equal(v, originalValue, "the saved value should be the same as the loaded value");
         done();
       })
   });
 
-  it('should save a object with a special key', function(done) {
+  it.skip('should save a object with a special key', function(done) {
       datastore.saveFullObject("userData", ["test"], function(err, v) {
         assert.equal(err, undefined);
         done();
