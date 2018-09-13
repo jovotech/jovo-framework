1bc70d0f5bfe6d2b193db8aaeb074fda0649f401
Matthew Lieder <lieder_matthew@hotmail.com>
2018-09-13 12:24:52 -0500

    Addresses issues around error handling with Azure Functions

--- examples/azure_functions/webhook/index.js
+++ examples/azure_functions/webhook/index.js
@@ -1,6 +1,14 @@
 'use strict';
 const {App} = require('../../../index');
-const app = new App();
+
+const config = {
+    // Local file database persistence shouldn't be used by Azure Functions,
+    // so we set the following property to disable persistence.
+    // In production you should either not use persistence or switch to a different database provider.
+    saveUserOnResponseEnabled: false
+};
+
+const app = new App(config);
 
 app.setHandler({
     'HelloIntent': function() {
@@ -12,5 +20,6 @@ app.setHandler({
 module.exports = function(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     app.handleFunction(context, req);
+    // context.done is called by the Jovo Framework so no need to do it here
 };
 
