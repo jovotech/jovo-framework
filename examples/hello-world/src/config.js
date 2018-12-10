9f007bf8cf83ee8b4399d4b6e0c4b4fde5124b8c
jovotech <a.swetlow@gmail.com>
2018-12-10 16:35:48 +0100

    :recycle: Fix hello-world example

--- examples/hello-world/src/config.js
+++ examples/hello-world/src/config.js
@@ -1,18 +1,6 @@
 module.exports = {
    logging: true,
-    userMetaData: {
-       enabled: true,
-    },
-   // v1: {
-   //    logging: true,
-   // },
-   //
-   // intentMap: {
-   //    'AMAZON.StopIntent': 'END',
-   // },
-   //
     db: {
-        default: 'DynamoDb',
         FileDb: {
             pathToFile: './db/db.json'
         }
