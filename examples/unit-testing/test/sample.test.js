aa19177cda999e4a3e160df11f2618eab9b445a6
jovotech <a.swetlow@gmail.com>
2018-12-13 10:27:53 +0100

    :recycle: Add GoogleAssistant to test suite

--- examples/unit-testing/test/sample.test.js
+++ examples/unit-testing/test/sample.test.js
@@ -4,7 +4,7 @@ const { GoogleAssistant } = require('jovo-platform-googleassistant');
 const { Alexa } = require('jovo-platform-alexa');
 jest.setTimeout(500);
 
-for (const p of [new Alexa()]) {
+for (const p of [new Alexa(), new GoogleAssistant()]) {
     const testSuite = p.makeTestSuite();
 
     describe(`PLATFORM: ${p.constructor.name} INTENTS` , () => {
@@ -12,7 +12,9 @@ for (const p of [new Alexa()]) {
             const conversation = testSuite.conversation();
 
             const launchRequest = await testSuite.requestBuilder.launch();
+
             const responseLaunchRequest = await conversation.send(launchRequest);
+
             expect(
                 responseLaunchRequest.isAsk('Hello World! What\'s your name?', 'Please tell me your name.')
             ).toBe(true);
