4df1eb7ddd9411478940c5356b906103429ee09d
Matthew Lieder <lieder_matthew@hotmail.com>
2019-01-02 14:30:35 -0600

    Image shouldn't be required for ListTemplate1

--- jovo-integrations/jovo-platform-alexa/src/response/visuals/ListTemplate1.ts
+++ jovo-integrations/jovo-platform-alexa/src/response/visuals/ListTemplate1.ts
@@ -3,7 +3,7 @@ import {Image, ImageShort, PlainText, RichText, Template, TextContent} from './T
 
 export interface ListItem {
     token: string;
-    image: Image | ImageShort;
+    image?: Image | ImageShort;
     textContent: TextContent;
 }
 
@@ -17,6 +17,9 @@ export class ListTemplate1 extends Template {
 /* eslint-enable */
 
     listItems: ListItem[] = [];
+
+    protected itemImageRequired = false;
+
     /**
      * Constructor
      * Sets type of template to 'BodyTemplate1'
@@ -26,11 +29,11 @@ export class ListTemplate1 extends Template {
     }
 
     addItem(listItem: ListItem): this;
-    addItem(token: string, image: string | Image | ImageShort, primaryText?: string | RichText | PlainText, secondaryText?: string | RichText | PlainText, tertiaryText?: string | RichText | PlainText): this;
+    addItem(token: string, image?: string | Image | ImageShort, primaryText?: string | RichText | PlainText, secondaryText?: string | RichText | PlainText, tertiaryText?: string | RichText | PlainText): this;
     addItem(tokenOrListItem: string | ListItem, image?: string | Image | ImageShort, primaryText?: string | RichText | PlainText, secondaryText?: string | RichText | PlainText, tertiaryText?: string | RichText | PlainText): this {
         if (typeof tokenOrListItem === 'string') {
 
-            if (!image) {
+            if (!image && this.itemImageRequired) {
                 throw new Error('Image is needed');
             }
 
@@ -38,16 +41,25 @@ export class ListTemplate1 extends Template {
                 throw new Error('At least primaryText is needed');
             }
 
-            this.listItems.push({
+            const listItem: ListItem = {
                 token: tokenOrListItem,
-                image: Template.makeImage(image),
                 textContent: Template.makeTextContent(
                     primaryText,
                     secondaryText,
                     tertiaryText
                 )
-            });
+            };
+
+            if (image) {
+                listItem.image = Template.makeImage(image);
+            }
+
+            this.listItems.push(listItem);
         } else {
+            if (!tokenOrListItem.image && this.itemImageRequired) {
+                throw new Error('Image is needed');
+            }
+
             this.listItems.push(tokenOrListItem);
         }
         return this;
