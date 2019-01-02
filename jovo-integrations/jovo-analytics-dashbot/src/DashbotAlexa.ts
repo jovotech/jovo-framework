c6deab1aec1b10cebb191c8ec07fa3a78cf09b80
jovotech <a.swetlow@gmail.com>
2019-01-02 15:43:36 +0100

    :bug: Fix dashbot import

--- jovo-integrations/jovo-analytics-dashbot/src/DashbotAlexa.ts
+++ jovo-integrations/jovo-analytics-dashbot/src/DashbotAlexa.ts
@@ -1,11 +1,11 @@
 import { Analytics, PluginConfig, BaseApp, HandleRequest } from "jovo-core";
 import _merge = require('lodash.merge');
-import dashbot, {AmazonAlexa} from 'dashbot';
+import {AmazonAlexa} from 'dashbot';
+import * as dashbot from 'dashbot';
 
 export interface Config extends PluginConfig {
     key: string;
 }
-
 
 export class DashbotAlexa implements Analytics {
     config: Config = {
@@ -21,6 +21,7 @@ export class DashbotAlexa implements Analytics {
     }
 
     install(app: BaseApp) {
+        // @ts-ignore
         this.dashbot = dashbot(this.config.key).alexa;
         app.on('response', this.track);
     }
