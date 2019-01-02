a11692328feda66427906aa0826653795b506e21
jovotech <a.swetlow@gmail.com>
2019-01-02 17:12:45 +0100

    :sparkles: Add js resource file compatibility

--- examples/i18n/src/i18n/de.js
+++ examples/i18n/src/i18n/de.js
@@ -1,7 +1,7 @@
-{
+module.exports = {
   "translation": {
     "WELCOME": "Willkommen",
     "WELCOME_WITH_PARAMETER": "Willkommen {{firstname}} {{lastname}}",
     "WELCOME_ARRAY": ["Willkommen", "Hallo"]
   }
-}
+};
