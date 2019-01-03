90cc939632086ee6c6f70ec5c1bc6a596f928faa
jovotech <a.swetlow@gmail.com>
2019-01-03 15:43:33 +0100

    :sparkes: Add helper methods to GoogleActionUser class

--- jovo-integrations/jovo-platform-googleassistant/src/core/GoogleActionUser.ts
+++ jovo-integrations/jovo-platform-googleassistant/src/core/GoogleActionUser.ts
@@ -37,7 +37,63 @@ export class GoogleActionUser extends User {
         return userId;
     }
 
+
+    /**
+     * Returns user profile after askForName permission
+     * @return {UserProfile}
+     */
     getProfile(): UserProfile {
         return _get(this.googleAction.$originalRequest, 'user.profile');
+    }
+
+    /**
+     * Returns granted permission array after askForPermission
+     * @return {string[]}
+     */
+    getPermissions(): string[] {
+        return _get(this.googleAction.$originalRequest, 'user.permissions');
+    }
+
+
+    /**
+     * Checks for permission
+     * @param {string} permission
+     * @return {boolean}
+     */
+    hasPermission(permission: string): boolean {
+        const permissions = this.getPermissions();
+
+        if (!permissions) {
+            return false;
+        }
+        return permissions.includes(permission);
+
+    }
+
+
+    /**
+     * Checks for name permission
+     * @return {boolean}
+     */
+    hasNamePermission(): boolean {
+        return this.hasPermission('NAME');
+    }
+
+
+    /**
+     * Checks for precise location permission
+     * @return {boolean}
+     */
+    hasPreciseLocationPermission(): boolean {
+        return this.hasPermission('DEVICE_PRECISE_LOCATION');
+    }
+
+
+    /**
+     * Checks for coarse location permission
+     * @return {boolean}
+     */
+    hasCoarseLocationPermission(): boolean {
+        return this.hasPermission('DEVICE_COARSE_LOCATION');
     }
 }
