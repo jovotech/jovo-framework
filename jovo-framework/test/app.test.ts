508b17ef5fb795bac61185a6a291567f4f457423
jovotech <a.swetlow@gmail.com>
2019-01-24 18:43:20 +0100

    :white_check_mark: Add tests for #656cc4b, #ade9bc5, #170cca5

--- jovo-framework/test/app.test.ts
+++ jovo-framework/test/app.test.ts
@@ -1,9 +1,42 @@
 import {App} from './../src/App';
 let app: App;
 jest.setTimeout(250);
+process.env.NODE_ENV = 'UNIT_TEST';
 
 
 app = new App();
-test('dummy test', () => {
-    expect(true).toBe(true);
+test('test onRequest', (done) => {
+
+    app.onRequest(() => {
+       done();
+    });
+
+    app.emit('request');
+});
+
+
+test('test onResponse', (done) => {
+
+    app.onResponse(() => {
+        done();
+    });
+
+    app.emit('response');
+});
+
+test('test onFail', (done) => {
+
+    app.onFail(() => {
+        done();
+    });
+
+    app.emit('fail');
+});
+test('test onError', (done) => {
+
+    app.onError(() => {
+        done();
+    });
+
+    app.emit('fail');
 });
