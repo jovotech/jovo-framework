d8e5628ca7bc1c577a026a0786d74ea0a55ddae9
jovotech <a.swetlow@gmail.com>
2019-02-26 14:20:45 +0100

    :recycle: Update return type

--- jovo-integrations/jovo-cms-i18next/src/index.ts
+++ jovo-integrations/jovo-cms-i18next/src/index.ts
@@ -1,14 +1,13 @@
 export {I18Next, Config} from './I18Next';
 declare module 'jovo-core/dist/src/Cms' {
     export interface Cms {
-        t(key: string, obj?: any): string | string[] | object | object[]; // tslint:disable-line
-        i18Next: any; // tslint:disable-line
+        t(key: string, obj?: any): string | string[]; // tslint:disable-line
     }
 }
 
 declare module 'jovo-core/dist/src/Jovo' {
     export interface Jovo {
-        t(key: string, obj?: any): string | string[] | object | object[]; // tslint:disable-line
+        t(key: string, obj?: any): string | string[]; // tslint:disable-line
     }
 }
 
