db86814279598725e684c6a51a12eb0a28832d6c
jovotech <a.swetlow@gmail.com>
2019-02-28 10:05:58 +0100

    :memo: Update jsdocs

--- jovo-core/src/BaseApp.ts
+++ jovo-core/src/BaseApp.ts
@@ -11,16 +11,17 @@ process.on('unhandledRejection', (reason, p) => {
     console.log(reason);
 });
 
-export type BaseAppMiddleware = 'setup' | 'request' | 'platform.init' | 'platform.nlu' | 'nlu' | 'user.load' | 'router' | 'handler' |
-    'user.save' | 'platform.output' | 'response' | 'fail';
-
 process.on('uncaughtException', (err) => {
     JovoError.printError(err as JovoError);
 });
 
+export type BaseAppMiddleware = 'setup' | 'request' | 'platform.init' | 'platform.nlu' | 'nlu' | 'user.load' | 'router' | 'handler' |
+    'user.save' | 'platform.output' | 'response' | 'fail';
+
 export interface BaseAppConfig extends ExtensibleConfig {
     inputMap?: {[key: string]: string};
 }
+
 // @ts-ignore
 process.env.JOVO_LOG_LEVEL = LogLevel.INFO;
 
@@ -92,16 +93,29 @@ export class BaseApp extends Extensible {
     }
 
 
-    getPlatformByName(name: string) {
+    /**
+     * Returns platform with given name.
+     * @param {string} name
+     * @returns {Platform | undefined}
+     */
+    getPlatformByName(name: string): Platform | undefined {
         return this.$platform.get(name);
     }
 
 
+    /**
+     * Emits webhook.init event.
+     */
     initWebhook() {
         this.emit('webhook.init');
     }
 
 
+    /**
+     * Hooks for the middleware handling.
+     * @param {BaseAppMiddleware} name
+     * @param {Function} func
+     */
     hook(name: BaseAppMiddleware, func: Function) {
         if (!this.middleware(name)) {
             throw new JovoError(
@@ -127,6 +141,11 @@ export class BaseApp extends Extensible {
     }
 
 
+    /**
+     * Main entry method which handles the request.
+     * @param {Host} host
+     * @returns {Promise<void>}
+     */
     async handle(host: Host) {
         const handleRequest: HandleRequest = {
             app: this,
@@ -140,10 +159,17 @@ export class BaseApp extends Extensible {
             // initialize on first call only
             if (!this.initialized) {
 
+                // First initialization of app object with first incoming request. Is executed once as long as app is alive
+                // Useful for api calls that take longer. Results can be saved in the app object for a simple caching.
                 await this.middleware('setup')!.run(handleRequest);
                 this.initialized = true;
             }
+
+
+            // Raw JSON request from platform gets processed. Can be used for authentication middlewares.
             await this.middleware('request')!.run(handleRequest);
+
+            // Determines which platform (e.g. Alexa, GoogleAssistant) sent the request. Initialization of abstracted jovo (this) object.
             await this.middleware('platform.init')!.run(handleRequest);
             if (!handleRequest.jovo) {
                 throw new JovoError(
@@ -160,28 +186,40 @@ export class BaseApp extends Extensible {
             Log.yellow().verbose(`this.$session.$data : ${JSON.stringify(handleRequest.jovo.$session.$data)}`);
             Log.verbose();
 
+            // 	Natural language understanding (NLU) information gets extracted for built-in NLUs (e.g. Alexa). Intents and inputs are set.
             await this.middleware('platform.nlu')!.run(handleRequest);
+
+            // Request gets routed through external NLU (e.g. Dialogflow standalone). Intents and inputs are set.
             await this.middleware('nlu')!.run(handleRequest);
 
             Log.verbose(Log.header('After nlu ', 'framework'));
             Log.yellow().verbose(`this.$nlu : ${JSON.stringify(handleRequest.jovo.$nlu)}`);
             Log.yellow().verbose(`this.$inputs : ${JSON.stringify(handleRequest.jovo.$inputs)}`);
 
+
+            // Initialization of user object. User data is retrieved from database.
             await this.middleware('user.load')!.run(handleRequest);
 
+
+            // Request and NLU data (intent, input, state) is passed to router. intentMap and inputMap are executed. Handler path is generated.
             await this.middleware('router')!.run(handleRequest);
 
+            // Handler logic is executed. Output object is created and finalized.
             await this.middleware('handler')!.run(handleRequest);
+
+            // User gets finalized, DB operations.
             await this.middleware('user.save')!.run(handleRequest);
 
 
             Log.white().verbose(Log.header(`Output object: this.$output`, 'framework'));
             Log.yellow().verbose(JSON.stringify(handleRequest.jovo.$output, null, '\t'));
+
+            // Platform response JSON gets created from output object.
             await this.middleware('platform.output')!.run(handleRequest);
 
 
             Log.verbose(Log.header('Response ', 'framework'));
-
+            // Response gets sent back to platform.
             await this.middleware('response')!.run(handleRequest);
         } catch (e) {
 
@@ -205,24 +243,40 @@ export class BaseApp extends Extensible {
         }
     }
 
+    /**
+     * On request listener
+     * @param {Function} callback
+     */
     onRequest(callback: Function) {
         this.on('request', (handleRequest: HandleRequest) => {
             callback(handleRequest);
         });
     }
 
+    /**
+     * On response listener
+     * @param {Function} callback
+     */
     onResponse(callback: Function) {
         this.on('response', (handleRequest: HandleRequest) => {
             callback(handleRequest);
         });
     }
 
+    /**
+     * On error listener. Same as the onFail listener
+     * @param {Function} callback
+     */
     onError(callback: Function) {
         this.on('fail', (handleRequest: HandleRequest) => {
             callback(handleRequest);
         });
     }
 
+    /**
+     * On fail listener
+     * @param {Function} callback
+     */
     onFail(callback: Function) {
         this.on('fail', (handleRequest: HandleRequest) => {
             callback(handleRequest);
@@ -238,6 +292,13 @@ export class BaseApp extends Extensible {
     }
 }
 
+
+/**
+ * Helper
+ * Returns array of parameter names from a function.
+ * @param {Function} func
+ * @returns {string[]}
+ */
 function getParamNames(func: Function): string[] {
     const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
     const ARGUMENT_NAMES = /([^\s,]+)/g;
