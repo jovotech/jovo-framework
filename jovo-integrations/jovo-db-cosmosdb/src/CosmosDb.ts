6d888f7d50e73c3a690c6fcdec050b757fa9ede6
Kaan Kilic <kilickaan1998@gmail.com>
2019-03-19 20:58:29 +0100

    :recycle: Adds config to constructor, adds install() to handle db.default logic

--- jovo-integrations/jovo-db-cosmosdb/src/CosmosDb.ts
+++ jovo-integrations/jovo-db-cosmosdb/src/CosmosDb.ts
@@ -1,7 +1,19 @@
-import {MongoDb} from 'jovo-db-mongodb';
+import {BaseApp} from 'jovo-core';
+import {MongoDb, Config} from 'jovo-db-mongodb';
+import _get = require('lodash.get');
 
 export class CosmosDb extends MongoDb {
-    constructor() {
-        super();
+    constructor(config?: Config) {
+        super(config);
+    }
+
+    install(app: BaseApp) {
+        if (_get(app.config, 'db.default')) {
+            if (_get(app.config, 'db.default') === 'CosmosDb') {
+                app.$db = this;
+            }
+        } else {
+            app.$db = this;
+        }
     }
 }
