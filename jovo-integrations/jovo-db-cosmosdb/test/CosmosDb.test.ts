d72154154fbdecfc496197bc393f5762cd789948
Kaan Kilic <kilickaan1998@gmail.com>
2019-03-28 23:29:48 +0100

    :white_check_mark: Fixes tests to match changes made in commit 95450b68ef3a6ee9763e84794b86604375a887fe

--- jovo-integrations/jovo-db-cosmosdb/test/CosmosDb.test.ts
+++ jovo-integrations/jovo-db-cosmosdb/test/CosmosDb.test.ts
@@ -1,34 +1,40 @@
 import { BaseApp } from "jovo-core";
 import { CosmosDb } from "../src/CosmosDb";
+import { MongoDb } from "jovo-db-mongodb";
 import _set = require('lodash.set');
 
 describe('test install()', () => {
     describe('test install() setting app.$db', () => {
-        test('test should set app.$db to be CosmosDb if no default db was set in config', () => {
+        beforeAll(() => {
+            // mock methods, which are called with super.install()
+            MongoDb.prototype.errorHandling = jest.fn();
+            MongoDb.prototype.install = jest.fn();
+        });
+        test('test should set app.$db to be CosmosDb if no default db was set in config', async () => {
             const cosmosdb = new CosmosDb();
             const app = new BaseApp();
 
-            cosmosdb.install(app);
+            await cosmosdb.install(app);
 
             expect(app.$db).toBeInstanceOf(CosmosDb);
         });
 
-        test('test app.$db should not be an instance of CosmosDb if default db set in config is not CosmosDb', () => {
+        test('test app.$db should not be an instance of CosmosDb if default db set in config is not CosmosDb', async () => {
             const cosmosdb = new CosmosDb();
             const app = new BaseApp();
             _set(app.config, 'db.default', 'test');
 
-            cosmosdb.install(app);
+            await cosmosdb.install(app);
 
             expect(app.$db).not.toBeInstanceOf(CosmosDb);
         });
 
-        test('test app.$db should be an instance CosmosDb if default db is set to CosmosDb', () => {
+        test('test app.$db should be an instance CosmosDb if default db is set to CosmosDb', async () => {
             const cosmosdb = new CosmosDb();
             const app = new BaseApp();
             _set(app.config, 'db.default', 'CosmosDb');
 
-            cosmosdb.install(app);
+            await cosmosdb.install(app);
 
             expect(app.$db).toBeInstanceOf(CosmosDb);
         });
