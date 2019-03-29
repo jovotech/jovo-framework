d84c2410ee9c52b2c52aeb7cc09c033d22705dd7
jovotech <a.swetlow@gmail.com>
2019-03-29 15:22:01 +0100

    :recycle: Update sheet range

--- jovo-integrations/jovo-cms-googlesheets/src/ObjectArraySheet.ts
+++ jovo-integrations/jovo-cms-googlesheets/src/ObjectArraySheet.ts
@@ -11,7 +11,7 @@ export interface Config extends GoogleSheetsSheet {
 export class ObjectArraySheet extends DefaultSheet {
     config: Config = {
         enabled: true,
-        range: 'A:B',
+        range: 'A:Z',
     };
     constructor(config?: Config) {
         super(config);
