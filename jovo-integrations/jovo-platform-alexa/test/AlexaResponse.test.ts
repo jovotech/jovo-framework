04ecc4a945a474a1860861bfa4b8d3bff5ce7c0a
Natrixx <natrixx@gmail.com>
2019-04-14 13:29:54 -0400

    add hasStandardCard and hasSimpleCard getters and tests

--- jovo-integrations/jovo-platform-alexa/test/AlexaResponse.test.ts
+++ jovo-integrations/jovo-platform-alexa/test/AlexaResponse.test.ts
@@ -22,7 +22,6 @@ test('test isTell', () => {
 
 });
 
-
 test('test isAsk', () => {
     const askResponse = AlexaResponse.fromJSON(_cloneDeep(askJSON));
     expect(askResponse.isAsk()).toBe(true);
@@ -40,5 +39,61 @@ test('test isAsk', () => {
 
 });
 
+test('test hasState', () => {
+    const responseWithState = AlexaResponse.fromJSON(_cloneDeep(askJSON));
+    responseWithState.sessionAttributes = {"_JOVO_STATE_":"test"};
 
+    expect(responseWithState.hasState('test')).toBe(true);
+    expect(responseWithState.hasState('test123')).toBe(false);
+    expect(responseWithState.hasState()).toBe(true);
 
+    const responseWithoutState = AlexaResponse.fromJSON(_cloneDeep(tellJSON));
+    expect(responseWithoutState.hasState('test123')).toBe(false);
+    expect(responseWithoutState.hasState()).toBe(false);
+});
+
+test('test hasSimpleCard', () => {
+    const tellResponse = AlexaResponse.fromJSON(_cloneDeep(tellJSON));
+    expect(tellResponse.hasSimpleCard()).toBe(true);
+
+    expect(tellResponse.hasSimpleCard('Simple Title')).toBe(true);
+    expect(tellResponse.hasSimpleCard('title')).toBe(false);
+
+    expect(tellResponse.hasSimpleCard('Simple Title', 'Simple Text')).toBe(true);
+    expect(tellResponse.hasSimpleCard('Simple Title', 'no')).toBe(false);
+});
+
+test('test hasStandardCard', () => {
+    const askResponse = AlexaResponse.fromJSON(_cloneDeep(askJSON));
+    expect(askResponse.hasStandardCard()).toBe(true);
+
+    expect(askResponse.hasStandardCard('Standard Title')).toBe(true);
+    expect(askResponse.hasStandardCard('title')).toBe(false);
+
+    expect(askResponse.hasStandardCard('Standard Title', 'Standard Text')).toBe(true);
+    expect(askResponse.hasStandardCard('Standard Title', 'no')).toBe(false);
+
+    expect(askResponse.hasStandardCard(
+        'Standard Title',
+        'Standard Text',
+        'https://via.placeholder.com/720x480'
+    )).toBe(true);
+    expect(askResponse.hasStandardCard(
+        'Standard Title',
+        'Standard Text',
+        'https://via.placeholder.com/'
+    )).toBe(false);
+
+    expect(askResponse.hasStandardCard(
+        'Standard Title',
+        'Standard Text',
+        'https://via.placeholder.com/720x480',
+        'https://via.placeholder.com/1200x800'
+    )).toBe(true);
+    expect(askResponse.hasStandardCard(
+        'Standard Title',
+        'Standard Text',
+        'https://via.placeholder.com/',
+        'https://via.placeholder.com/'
+    )).toBe(false);
+});
