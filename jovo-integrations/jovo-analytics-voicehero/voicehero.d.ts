e3e3df6b2a54b9a5f291f1b901f906ae7740193d
Jacob Chan <jacob.chan5@gmail.com>
2019-09-10 10:45:44 -0700

    add voicehero integration

---
+++ jovo-integrations/jovo-analytics-voicehero/voicehero.d.ts
@@ -0,0 +1,11 @@
+declare module 'voicehero-sdk' {
+    export interface Platform {
+        logIncoming(request: object): void;
+        logOutgoing(request: object, response: object): void;
+    }
+
+    export interface AmazonAlexa extends Platform {
+    }
+
+    export default function (key: string): { alexa: AmazonAlexa };
+}
