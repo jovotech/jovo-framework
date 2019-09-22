e0f2d5157822698e6935e0e8c799e65d2acc3193
Phil Krylov <phil.krylov@gmail.com>
2019-09-22 20:10:19 +0300

    Fix GoogleActionSpeechBuilder.addAudio() to match docs+implementation+common sense

--- jovo-platforms/jovo-platform-googleassistant/src/core/GoogleActionSpeechBuilder.ts
+++ jovo-platforms/jovo-platform-googleassistant/src/core/GoogleActionSpeechBuilder.ts
@@ -1,3 +1,5 @@
+import _sample = require('lodash.sample');
+import _zip = require('lodash.zip');
 
 import {SpeechBuilder} from "jovo-core";
 import {GoogleAction} from "./GoogleAction";
@@ -9,26 +11,30 @@ export class GoogleActionSpeechBuilder extends SpeechBuilder {
     /**
      * Adds audio tag to speech
      * @public
-     * @param {string} url secure url to audio
-     * @param {string} text
+     * @param {string | string[]} url secure url to audio
+     * @param {string | string[]} text
      * @param {boolean} condition
      * @param {number} probability
      * @return {SpeechBuilder}
      */
-    addAudio(url: string, text = '', condition?: boolean, probability?: number) {
+    addAudio(url: string | string[], text?: string | string[], condition?: boolean, probability?: number) {
         // gets random element from array if url
         // is of type array
-        if (Array.isArray(url)) {
-            const rand = Math.floor(Math.random() * url.length);
-            url = url[rand];
+        if (Array.isArray(url) && Array.isArray(text) && text.length === url.length) {
             // takes the same index from the text array
-            if (!text) {
-                text = url;
-            } else {
-                text = text[rand];
+            [url, text] = (_sample(_zip(url, text)) as string[]);
+        } else {
+            if (Array.isArray(url)) {
+                url = (_sample(url) as string);
+            }
+            if (Array.isArray(text)) {
+                text = (_sample(text) as string);
             }
         }
-        return this.addText('<audio src="' + url + '">' + text + '</audio>', condition, probability);
+        if (!text) {
+            text = '';
+        }
+        return this.addText('<audio src="' + url + '"' + ((text.length > 0) ? '>' + text + '</audio>' : '/>'), condition, probability);
     }
 
     /**
