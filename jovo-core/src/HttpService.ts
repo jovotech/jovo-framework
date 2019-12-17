9f96d305d8fb8650c21d40f06cf5a9b9043e0448
m-ripper <max@jovo.tech>
2019-12-17 15:44:53 +0100

    :sparkles: Implemented HttpService

---
+++ jovo-core/src/HttpService.ts
@@ -0,0 +1,68 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export class HttpService {
+  static get axios(): AxiosInstance {
+    return axios;
+  }
+
+  // tslint:disable-next-line:no-any
+  static request<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    config: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.request(config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static get<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.get(url, config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static delete<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.delete(url, config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static head<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.head(url, config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static post<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    // tslint:disable-next-line:no-any
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.post(url, data, config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static put<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    // tslint:disable-next-line:no-any
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.put(url, data, config);
+  }
+
+  // tslint:disable-next-line:no-any
+  static patch<VALUE = any, RESPONSE = AxiosResponse<VALUE>>(
+    url: string,
+    // tslint:disable-next-line:no-any
+    data?: any,
+    config?: AxiosRequestConfig,
+  ): Promise<RESPONSE> {
+    return axios.patch(url, data, config);
+  }
+}
