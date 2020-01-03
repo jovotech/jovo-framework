d5c8eddda17c455eec947ef18cd970dc36b70e63
jovotech <a.swetlow@gmail.com>
2020-01-03 17:41:20 +0100

    :recycle: Add getGoogleProfile() method on ON_SIGN_IN

--- jovo-platforms/jovo-platform-googleassistant/src/core/GoogleActionUser.ts
+++ jovo-platforms/jovo-platform-googleassistant/src/core/GoogleActionUser.ts
@@ -1,11 +1,23 @@
-import { User } from 'jovo-core';
+import { JovoError, User } from 'jovo-core';
 import _get = require('lodash.get');
 import { GoogleAction } from './GoogleAction';
+import * as https from 'https';
 
 export interface UserProfile {
   displayName: string;
   givenName: string;
   familyName: string;
+}
+
+export interface GoogleAccountProfile {
+  email: string;
+  email_verified: boolean;
+  name: string;
+  picture: string;
+  given_name: string;
+  family_name: string;
+
+  [key: string]: string | boolean;
 }
 
 export class GoogleActionUser extends User {
@@ -77,5 +89,50 @@ export class GoogleActionUser extends User {
    */
   hasCoarseLocationPermission(): boolean {
     return this.hasPermission('DEVICE_COARSE_LOCATION');
+  }
+
+  async getGoogleProfile(): Promise<GoogleAccountProfile> {
+    const tokenId = _get(this.googleAction.$originalRequest, 'user.idToken');
+
+    if (!tokenId) {
+      throw new JovoError('No token found.!');
+    }
+
+    return new Promise((resolve, reject) => {
+      const options = {
+        host: 'oauth2.googleapis.com',
+        port: 443,
+        path: `/tokeninfo?id_token=${tokenId}`,
+        timeout: 2000,
+        protocol: 'https:'
+      };
+
+      const req = https.get(options, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+          try {
+            const googleAccount = JSON.parse(data) as GoogleAccountProfile;
+            return resolve(googleAccount);
+          } catch(e) {
+            reject(e);
+          }
+        });
+
+      }).on("error", (err) => {
+        reject(err);
+      });
+      req.on('timeout', () => {
+        req.abort();
+      });
+    });
+
+
   }
 }
