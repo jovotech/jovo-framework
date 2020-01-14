7fce3b07b493e25e8011d18429185ea291f2d243
Max Ripper <Max@jovo.tech>
2020-01-14 14:08:01 +0100

    :tada: Added js & ts example for the facebookmessenger-platform

---
+++ examples/javascript/facebook-messenger-platform/src/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const { App } = require('jovo-framework');
+const { FacebookMessenger } = require('jovo-platform-facebookmessenger');
+const { LuisNlu } = require('jovo-nlu-luis');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+const app = new App();
+
+const messenger = new FacebookMessenger({
+	verifyToken: 'VerificationToken'
+});
+
+messenger.use(
+	new LuisNlu({
+		appId: process.env.LUIS_APP_ID,
+		endpointHost: 'westus.api.cognitive.microsoft.com',
+		endpointKey: process.env.LUIS_ENDPOINT_KEY
+	})
+);
+
+app.use(messenger, new JovoDebugger(), new FileDb());
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+	LAUNCH() {
+		return this.toIntent('HelloWorldIntent');
+	},
+
+	HelloWorldIntent() {
+		this.ask("Hello World! What's your name?", 'Please tell me your name.');
+	},
+
+	MyNameIsIntent() {
+		console.log(this.$request.getInputs());
+
+		this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+	}
+});
+
+module.exports.app = app;
