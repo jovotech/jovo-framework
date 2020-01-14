4479a9bfa55487a1147782b5c3a2a5f779a4eee0
Kaan Kilic <kilickaan1998@gmail.com>
2020-01-14 16:50:00 +0100

    :memo: adds examples for amazon pay integration

---
+++ examples/javascript/01_alexa/amazon-pay/src/app.js
@@ -0,0 +1,101 @@
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new JovoDebugger(),
+);
+
+app.setHandler({
+
+    LAUNCH() {
+        if (this.$alexaSkill.isAmazonPayPermissionDenied()) {
+            this.$alexaSkill.showAskForAmazonPayPermissionCard();
+        } else {
+            return this.toIntent('GetBuyerId');
+            // return this.toIntent('SetupDirective');
+            // return this.toIntent('ChargeDirective');
+            // return this.toIntent('GetBuyerAddress');
+        }
+    },
+
+    SetupDirective() {
+        const payload = this.$alexaSkill.$pay.createSetupPayload()
+            .setSellerId('YOUR SELLER ID')
+            .setCountryOfEstablishment('DE')
+            .setLedgerCurrency('EUR')
+            .setCheckoutLanguage('en_US')
+            .setSellerBillingAgreementId('id')
+            .setBillingAgreementType('CustomerInitiatedTransaction')
+            .setSubscriptionAmount('19.99')
+            .setSubscriptionCurrencyCode('EUR')
+            .setStoreName('Test name')
+            .setCustomInformation('custom info')
+            .setNeedAmazonShippingAddress(true)
+            .setSandboxMode(true)
+            .setSandboxEmail('test@jovo.tech')
+            .build();
+        
+        const directive = {
+            type: 'Connections.SendRequest',
+            name: 'Setup',
+            token: 'token',
+            payload,
+        };
+
+        return this.$alexaSkill.addDirective(directive);
+    },
+
+    ChargeDirective() {
+        const payload = this.$alexaSkill.$pay.createChargePayload()
+            .setSellerId('YOUR SELLER ID')
+            .setBillingAgreementId('billing id')
+            .setPaymentAction('AuthorizeAndCapture')
+            .setAuthorizationReferenceId('reference id')
+            .setAuthorizationAmount('19.99')
+            .setAuthorizationCurrencyCode('EUR')
+            .setSoftDescriptor('description')
+            .setTransactionTimeout('0')
+            .setSellerAuthorizationNote('note')
+            .setStoreName('name')
+            .setCustomInformation('custom info')
+            .setSellerNote('sellerNote')
+            .setSellerOrderId('order id')
+            .build()
+
+        const directive = {
+            type: 'Connections.SendRequest',
+            name: 'Charge',
+            token: 'token',
+            payload,
+        };
+
+        return this.$alexaSkill.addDirective(directive);
+    },
+
+    async GetBuyerId() {
+        const response = await this.$alexaSkill.$user.getBuyerId();
+        console.log(response);
+
+        return this.tell('Done!');
+    },
+
+    async GetBuyerAddress() {
+        const options = {
+            sellerId: 'YOUR SELLER ID',
+            sandbox: true,
+            sandBoxEmail: 'YOUR TEST ACCOUNT EMAILs'
+        };
+
+        const response = await this.$alexaSkill.$user.getDefaultBuyerAddress(options);
+        console.log(response);
+
+        return this.tell('Done!');
+    }
+});
+
+
+module.exports.app = app;
