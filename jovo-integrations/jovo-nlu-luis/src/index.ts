186b6f81dcb83db0cbbbcbc7535c23b6684b3a06
Max Ripper <Max@jovo.tech>
2020-02-10 16:45:14 +0100

    :sparkles: Added improved config-types for new packages

--- jovo-integrations/jovo-nlu-luis/src/index.ts
+++ jovo-integrations/jovo-nlu-luis/src/index.ts
@@ -1,2 +1,12 @@
+import { Config } from './LuisNlu';
+
+interface AppLuisNluConfig {
+	LuisNlu?: Config;
+}
+
+declare module 'jovo-core/dist/src/Interfaces' {
+	export interface AppNluConfig extends AppLuisNluConfig {}
+	export interface ExtensiblePluginConfigs extends AppLuisNluConfig {}
+}
 export * from './LuisNlu';
 export * from './Interfaces';
--- jovo-integrations/jovo-tts-gcloud/src/index.ts
+++ jovo-integrations/jovo-tts-gcloud/src/index.ts
@@ -1,2 +1,24 @@
+import { Config } from './GCloudTts';
+
+interface AppGCloudTtsConfig {
+	GCloudTts?: Config;
+}
+
+declare module 'jovo-core/dist/src/Interfaces' {
+	export interface ExtensiblePluginConfigs extends AppGCloudTtsConfig {}
+}
+
+declare module 'jovo-core/dist/src/Interfaces' {
+	interface TellOutput {
+		speechText?: string;
+	}
+
+	interface AskOutput {
+		speechText?: string;
+		repromptText?: string;
+	}
+}
+
+
 export * from './GCloudTts';
 export * from './Interfaces';
