cc9bd47570971ef05bdfdc3e6f8a8eb7476b457e
Max Ripper <Max@jovo.tech>
2020-02-12 15:16:41 +0100

    :art: Cleanup FacebookMessenger

--- jovo-platforms/jovo-platform-facebookmessenger/src/responses/quick-replies/TextQuickReply.ts
+++ jovo-platforms/jovo-platform-facebookmessenger/src/responses/quick-replies/TextQuickReply.ts
@@ -3,7 +3,7 @@ import { QuickReply, QuickReplyContentType } from '../..';
 export class TextQuickReply implements QuickReply {
   content_type: QuickReplyContentType;
 
-  constructor(public title: string, public payload: string | number, public imageUrl?: string) {
+  constructor(public title: string, public payload: string | number = title, public imageUrl?: string) {
     this.content_type = QuickReplyContentType.Text;
   }
 }
