1e8b440289f143e17298e0935f9b8109d21ce5dc
jovotech <a.swetlow@gmail.com>
2020-03-23 15:11:45 +0100

    :refactor: Refactor interactive canvas demo

--- examples/typescript/02_googleassistant/interactive-canvas/webapp/server.js
+++ examples/typescript/02_googleassistant/interactive-canvas/webapp/server.js
@@ -1,8 +1,10 @@
 "use strict";
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 const app = express();
-
+app.use(express.static(__dirname + '/public'));
+app.use(cors())
 app.get("/", (req, res) => {
     res.sendFile(path.join(process.cwd(), 'webapp/public/canvas.html'));
 });
