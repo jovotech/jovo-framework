1f5efecb823d17d84fd2168168981818e0a39605
Max Ripper <Max@jovo.tech>
2020-04-29 17:26:07 +0200

    :bug: Fixed audio output for iOS
    Side-effect: Now only one audio file can be played at a time

--- jovo-clients/jovo-client-web/src/core/AudioPlayer.ts
+++ jovo-clients/jovo-client-web/src/core/AudioPlayer.ts
@@ -1,167 +1,78 @@
 import { AudioPlayerEvents, Base64Converter, CoreComponent } from '..';
-import { AudioPlayback } from './Interfaces';
 
 export class AudioPlayer extends CoreComponent {
-  readonly name = 'AudioPlayer';
-  private $volume = 1.0;
-  private activePlaybacks: AudioPlayback[] = [];
-  private idCounter = 0;
+	readonly name = 'AudioPlayer';
+	private $volume = 1.0;
+	private audio: HTMLAudioElement | null = null;
+	private isAudioPlaying: boolean = false;
 
-  get isPlaying(): boolean {
-    return this.activePlaybacks.some((playback) => {
-      return !playback.audio.paused && !playback.audio.ended;
-    });
-  }
+	get isPlaying(): boolean {
+		return this.isAudioPlaying;
+	}
 
-  get volume(): number {
-    return this.$volume;
-  }
+	get volume(): number {
+		return this.$volume;
+	}
 
-  set volume(value: number) {
-    this.$volume = value;
-    for (let i = 0, len = this.activePlaybacks.length; i < len; i++) {
-      this.activePlaybacks[i].audio.volume = value;
-    }
-  }
+	set volume(value: number) {
+		this.$volume = value;
+		if (this.audio) {
+			this.audio.volume = value;
+		}
+	}
 
-  play(audioSource: string): Promise<void> {
-    if (audioSource.startsWith('https://')) {
-      return this.setupAudioAndPlay(audioSource);
-    } else {
-      return this.playBase64Encoded(audioSource, 'audio/mpeg');
-    }
-  }
+	init() {
+		const audio = new Audio('');
 
-  resume(id: number) {
-    const index = this.findPlaybackIndex(id);
-    if (index >= 0) {
-      this.resumePlayback(this.activePlaybacks[index]);
-    }
-  }
+		audio.play().then(() => {
+			audio.pause();
+		});
 
-  resumeAll() {
-    for (let i = 0, len = this.activePlaybacks.length; i < len; i++) {
-      this.resumePlayback(this.activePlaybacks[i]);
-    }
-  }
+		audio.onended = () => {
+			this.isAudioPlaying = false;
+			this.$client.emit(AudioPlayerEvents.End);
+		};
+		this.audio = audio;
+	}
 
-  pause(id: number) {
-    const index = this.findPlaybackIndex(id);
-    if (index >= 0) {
-      this.pausePlayback(this.activePlaybacks[index]);
-    }
-  }
+	resume() {
+		if (this.audio && !this.audio.ended && this.audio.paused) {
+			this.audio.play().then(() => {
+				this.isAudioPlaying = true;
+				this.$client.emit(AudioPlayerEvents.Resume);
+			});
+		}
+	}
 
-  pauseAll() {
-    for (let i = 0, len = this.activePlaybacks.length; i < len; i++) {
-      this.pausePlayback(this.activePlaybacks[i]);
-    }
-  }
+	pause() {
+		if (this.audio && !this.audio.paused && !this.audio.ended) {
+			this.audio.pause();
+			this.isAudioPlaying = false;
+			this.$client.emit(AudioPlayerEvents.Pause);
+		}
+	}
 
-  stop(id: number) {
-    const index = this.findPlaybackIndex(id);
-    if (index >= 0) {
-      this.stopPlayback(this.activePlaybacks[index]);
-    }
-  }
+	stop() {
+		if (this.audio && !this.audio.ended) {
+			this.audio.pause();
+			this.audio.currentTime = 0;
+			this.isAudioPlaying = false;
+			this.$client.emit(AudioPlayerEvents.Stop);
+		}
+	}
 
-  stopAll() {
-    for (let i = 0, len = this.activePlaybacks.length; i < len; i++) {
-      this.stopPlayback(this.activePlaybacks[i]);
-    }
-  }
+	async play(audioSource: string, contentType = 'audio/mpeg'): Promise<void> {
+		if (!this.audio) {
+			throw new Error('The AudioPlayer has to be initialized before being able to play audio.');
+		}
+		if (!audioSource.startsWith('https://')) {
+			const blob = await Base64Converter.base64ToBlob(audioSource, contentType);
+			audioSource = URL.createObjectURL(blob);
+		}
 
-  private playBase64Encoded(base64Audio: string, contentType: string): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const blob = await Base64Converter.base64ToBlob(base64Audio, contentType);
-        const url = URL.createObjectURL(blob);
-        resolve(await this.setupAudioAndPlay(url));
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  private setupAudioAndPlay(audioSource: string): Promise<void> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const audio: HTMLAudioElement = new Audio(audioSource);
-        audio.volume = this.volume;
-        const id = this.idCounter;
-
-        audio.onerror = (e) => {
-          this.$client.emit(AudioPlayerEvents.Error, e);
-          reject(e);
-        };
-
-        audio.onpause = () => {
-          const playback = this.findPlayback(id);
-          if (playback && playback.stopped) {
-            this.removePlayback(id);
-            resolve();
-          }
-        };
-
-        audio.onended = () => {
-          this.removePlayback(id);
-          this.$client.emit(AudioPlayerEvents.End, id);
-          resolve();
-        };
-
-        await audio.play();
-        this.$client.emit(AudioPlayerEvents.Play, id, audioSource);
-
-        this.activePlaybacks.push({
-          audio,
-          id,
-        });
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  private resumePlayback(playback: AudioPlayback) {
-    if (!playback.audio.ended && playback.audio.paused) {
-      playback.audio.play().then(() => {
-        this.$client.emit(AudioPlayerEvents.Resume, playback.id);
-      });
-    }
-  }
-
-  private pausePlayback(playback: AudioPlayback) {
-    if (!playback.audio.paused && !playback.audio.ended) {
-      playback.audio.pause();
-      this.$client.emit(AudioPlayerEvents.Pause, playback.id);
-    }
-  }
-
-  private stopPlayback(playback: AudioPlayback) {
-    if (!playback.audio.paused && !playback.audio.ended) {
-      playback.audio.pause();
-      playback.audio.currentTime = 0;
-      playback.stopped = true;
-      this.$client.emit(AudioPlayerEvents.Stop, playback.id);
-    }
-  }
-
-  private removePlayback(id: number) {
-    const index = this.findPlaybackIndex(id);
-    if (index >= 0) {
-      this.activePlaybacks.splice(index, 1);
-    }
-  }
-
-  private findPlayback(id: number): AudioPlayback | undefined {
-    return this.activePlaybacks.find((playback: AudioPlayback) => {
-      return playback.id === id;
-    });
-  }
-
-  private findPlaybackIndex(id: number): number {
-    return this.activePlaybacks.findIndex((playback: AudioPlayback) => {
-      return playback.id === id;
-    });
-  }
+		this.audio.src = audioSource;
+		await this.audio.play();
+		this.isAudioPlaying = true;
+		this.$client.emit(AudioPlayerEvents.Play, audioSource);
+	}
 }
