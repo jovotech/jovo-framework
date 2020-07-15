f97b4850780723aa6e5a90f8fb34d59e8bbf5623
Kaan Kilic <kilickaan1998@gmail.com>
2020-07-15 22:06:05 +0200

    :bug: Business Messages: uses session ID as user ID

--- jovo-platforms/jovo-platform-google-business-messages/src/core/BusinessMessagesUser.ts
+++ jovo-platforms/jovo-platform-google-business-messages/src/core/BusinessMessagesUser.ts
@@ -1,18 +1,16 @@
-import { User, Util } from 'jovo-core';
+import { User } from 'jovo-core';
 import { BusinessMessagesBot } from './BusinessMessagesBot';
+import { BusinessMessagesRequest } from './BusinessMessagesRequest';
 
 export class BusinessMessagesUser extends User {
   businessMessagesBot: BusinessMessagesBot;
-  id: string;
 
   constructor(businessMessagesBot: BusinessMessagesBot) {
     super(businessMessagesBot);
     this.businessMessagesBot = businessMessagesBot;
-    // platform doesn't provide a user ID. To save data in the db, etc. we need an ID tho.
-    this.id = Util.randomStr(12);
   }
 
   getId(): string {
-    return this.id;
+    return (this.businessMessagesBot.$request as BusinessMessagesRequest).getSessionId();
   }
 }
