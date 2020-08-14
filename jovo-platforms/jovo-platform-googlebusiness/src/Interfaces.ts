feda4838115d85e316c2ed1a4951bcecef7bd341
Max Ripper <Max@jovo.tech>
2020-08-14 15:28:33 +0200

    🏷️ Improve typings

--- jovo-platforms/jovo-platform-googlebusiness/src/Interfaces.ts
+++ jovo-platforms/jovo-platform-googlebusiness/src/Interfaces.ts
@@ -1,10 +1,12 @@
+export type EntryPoint = 'ENTRY_POINT_UNSPECIFIED' | 'PLACESHEET' | 'MAPS';
+
 export interface GoogleBusinessBaseRequest {
   agent: string;
   conversationId: string;
   customAgentId: string;
   requestId: string;
   context?: {
-    entryPoint: 'ENTRY_POINT_UNSPECIFIED' | 'PLACESHEET' | 'MAPS';
+    entryPoint: EntryPoint;
     placeId: string;
     userInfo: {
       displayName: string;
@@ -22,13 +24,15 @@ export interface GoogleBusinessMessageRequest extends GoogleBusinessBaseRequest 
   };
 }
 
+export type SuggestionType = 'UNKNOWN' | 'ACTION' | 'REPLY';
+
 export interface GoogleBusinessSuggestionRequest extends GoogleBusinessBaseRequest {
   suggestionResponse: {
     message: string;
     postbackData: string;
     createTime: string; // RFC3339 UTC "Zulu" format
     text: string;
-    suggestionType: 'UNKNOWN' | 'ACTION' | 'REPLY';
+    suggestionType: SuggestionType;
   };
 }
 
@@ -37,12 +41,13 @@ export interface ResponseOptions {
   fallback?: string;
 }
 
+export type RepresentativeType = 'REPRESENTATIVE_TYPE_UNSPECIFIED' | 'BOT' | 'HUMAN';
 
 export interface BaseResponse extends ResponseOptions {
   messageId: string;
   representative: {
     displayName?: string;
-    representativeType: 'REPRESENTATIVE_TYPE_UNSPECIFIED' | 'BOT' | 'HUMAN';
+    representativeType: RepresentativeType;
   };
 }
 
@@ -95,8 +100,11 @@ export interface CarouselCardResponse extends BaseResponse {
   };
 }
 
+export type CardWidth = 'CARD_WIDTH_UNSPECIFIED' | 'SMALL' | 'MEDIUM';
+export type CardHeight = 'HEIGHT_UNSPECIFIED' | 'SHORT' | 'MEDIUM' | 'TALL';
+
 export interface CarouselCard {
-  cardWidth: 'CARD_WIDTH_UNSPECIFIED' | 'SMALL' | 'MEDIUM';
+  cardWidth: CardWidth;
   cardContents: Card[];
 }
 
@@ -104,7 +112,7 @@ export interface Card {
   title?: string;
   description?: string;
   media?: {
-    height: 'HEIGHT_UNSPECIFIED' | 'SHORT' | 'MEDIUM' | 'TALL';
+    height: CardHeight;
     contentInfo: {
       fileUrl: string;
       thumbnailUrl?: string;
