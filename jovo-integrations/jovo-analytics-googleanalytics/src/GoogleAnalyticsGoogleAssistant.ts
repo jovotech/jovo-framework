772faf4dcd3b814d33018e3566b201ffb2c6906e
freiSMS <andre.heraucourt@web.de>
2020-08-24 12:50:55 +0200

    :sparkles: filter bots and no voicematch users

--- jovo-integrations/jovo-analytics-googleanalytics/src/GoogleAnalyticsGoogleAssistant.ts
+++ jovo-integrations/jovo-analytics-googleanalytics/src/GoogleAnalyticsGoogleAssistant.ts
@@ -1,6 +1,8 @@
 import { GoogleAnalytics } from './GoogleAnalytics';
 import { GoogleActionRequest } from 'jovo-platform-googleassistant';
+import { DialogflowRequest } from 'jovo-platform-dialogflow'
 import { HandleRequest, Jovo, JovoError, ErrorCode } from 'jovo-core';
+import { get } from 'lodash';
 
 export class GoogleAnalyticsGoogleAssistant extends GoogleAnalytics {
   track(handleRequest: HandleRequest) {
@@ -17,6 +19,19 @@ export class GoogleAnalyticsGoogleAssistant extends GoogleAnalytics {
       return;
     }
 
+    const dialogFlowRequest = jovo.$request as DialogflowRequest;
+
+    const userName: string | undefined = get(dialogFlowRequest.originalDetectIntentRequest!.payload, 'user.profile.familyName');
+    const isCrawler = userName && userName === 'Crawler';
+    if (isCrawler) {
+      return;
+    }
+
+    const userVerificationStatus: string | undefined = get(dialogFlowRequest.originalDetectIntentRequest!.payload, 'user.userVerificationStatus');//  inputs[0].rawInputs[0].inputType');
+    const isVoiceMatchUser = userVerificationStatus && userVerificationStatus === 'VERIFIED';
+    if (!isVoiceMatchUser) {
+      return;
+    }
     super.track(handleRequest);
   }
 
@@ -26,7 +41,7 @@ export class GoogleAnalyticsGoogleAssistant extends GoogleAnalytics {
     const request = jovo.$request as GoogleActionRequest;
     const deviceInfo = `Google Assistant Device - ${
       request.hasScreenInterface() ? 'Display Support' : 'Voice Only'
-    }`;
+      }`;
 
     // fake UserAgent which makes GA mappping device to browser field and platform type to mobile
     this.visitor!.set(
