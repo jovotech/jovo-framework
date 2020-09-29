7949a6ee8ad9b655dffbf36ff203dcb0e96503b7
Max Ripper <max@jovo.tech>
2020-09-29 12:19:50 +0200

    :wrench: :hammer: Update webpack config for web-client

--- jovo-clients/jovo-client-web/webpack.config.js
+++ jovo-clients/jovo-client-web/webpack.config.js
@@ -1,43 +1,59 @@
 const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const analyzeBundle = process.env.ANALYZE_BUNDLE || false;
+module.exports = (env, ctx) => {
+  function createConfig(target, targetName = target) {
+    const plugins = [new CleanWebpackPlugin({ cleanStaleWebpackAssets: false })];
 
-const plugins = [];
-if (analyzeBundle) {
-  plugins.push(
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      analyzerHost: 'localhost',
-      analyzerPort: 4000,
-    }),
-  );
-}
+    if (ctx.mode === 'development' && target === 'umd') {
+      plugins.push(
+        new HtmlWebpackPlugin({
+          title: 'Jovo Web Client',
+          template: 'index.html',
+        }),
+      );
+    }
 
-function createConfig(target, targetName = target) {
-  return {
-    entry: './src/index.ts',
-    devtool: process.env.NODE_ENV === 'development' ? 'inline-source-map' : '',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: `index.${targetName}.js`,
-      library: 'JovoWebClient',
-      libraryTarget: target,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    plugins,
-  };
-}
+    if (env && env.ANALYZE_BUNDLE === 'true') {
+      plugins.push(
+        new BundleAnalyzerPlugin({
+          reportFilename: `report-${targetName}.html`,
+          analyzerMode: 'static',
+          analyzerHost: 'localhost',
+          analyzerPort: 4000,
+        }),
+      );
+    }
+    return {
+      mode: ctx.mode || 'production',
+      entry: './src/index.ts',
+      devtool: ctx.mode === 'development' ? 'inline-source-map' : 'source-map',
+      devServer: {
+        contentBase: './dist',
+      },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: `index.${targetName}.js`,
+        library: 'JovoWebClient',
+        libraryTarget: target,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.ts', '.js'],
+      },
+      plugins,
+    };
+  }
 
-module.exports = [createConfig('commonjs2', 'common'), createConfig('umd')];
+  return [createConfig('umd'), createConfig('commonjs2', 'common')];
+};
