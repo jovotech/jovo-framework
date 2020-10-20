84732512d8e54e5613bc74eaa4fa06d76c4cc5b1
Dominik Meißner <dm@nuware.de>
2020-10-20 08:29:37 +0200

    Genesys integration v1

--- jovo-platforms/jovo-platform-dialogflow/src/DialogflowAgent.ts
+++ jovo-platforms/jovo-platform-dialogflow/src/DialogflowAgent.ts
@@ -3,7 +3,7 @@ import _get = require('lodash.get');
 import _set = require('lodash.set');
 import { DialogflowUser } from './DialogflowUser';
 
-export type SupportedIntegration = 'FacebookMessenger' | 'Slack' | 'DialogflowPhoneGateway';
+export type SupportedIntegration = 'FacebookMessenger' | 'Slack' | 'Twilio' | 'DialogflowPhoneGateway' | 'Genesys';
 
 export class DialogflowAgent extends Jovo {
   $dialogflowAgent: DialogflowAgent;
---
+++ jovo-platforms/jovo-platform-dialogflow/src/integrations/Genesys/GenesysUser.ts
@@ -0,0 +1,33 @@
+import { Jovo } from 'jovo-core';
+import _get = require('lodash.get');
+import { DialogflowUser } from '../../DialogflowUser';
+
+export class GenesysUser extends DialogflowUser {
+  constructor(jovo: Jovo) {
+    super(jovo);
+  }
+
+  getAccessToken(): string | undefined {
+    return undefined;
+  }
+
+  getId(): string {
+    // the original Payload does not come with a id like Caller-ID.
+    // TODO: A call to the Genesys API can get us caller ID
+    // purecloud-platform-client-v2.ConversationsApi.getAnalyticsConversationDetails(conversationId);
+    // ani: response.participants[0].sessions[0].ani
+    return "GenericGenesysUser"
+  }
+
+  getNoInputLimit(): number | undefined {
+    return _get(this.jovo.$request, 'originalDetectIntentRequest.payload.Genesys-No-Input-Limit');
+  }
+
+  getConversattionId(): number | undefined {
+    return _get(this.jovo.$request, 'originalDetectIntentRequest.payload.Genesys-Conversation-Id');
+  }
+
+  getNoInputCount(): number | undefined {
+    return _get(this.jovo.$request, 'originalDetectIntentRequest.payload.Genesys-No-Input-Count');
+  }
+}
