a387423d37ec77f628327d6beb341fc7d4771888
Max Ripper <max@jovo.tech>
2020-10-21 21:36:13 +0200

    :bug: Fix duplicate export

--- jovo-platforms/jovo-platform-web/src/index.ts
+++ jovo-platforms/jovo-platform-web/src/index.ts
@@ -1,6 +1,6 @@
+import { Config } from 'jovo-core';
 import { Action } from 'jovo-platform-core';
 import { WebApp } from './core/WebApp';
-import { Config } from './WebPlatform';
 
 export * from 'jovo-platform-core';
 export * from './WebPlatform';
