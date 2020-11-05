49aa57405c8fb6197e9f3967b5ada537cef54fcc
Max Ripper <max@jovo.tech>
2020-11-05 17:33:35 +0100

    :bug: Fix export, change from core-platform to web-platform

--- jovo-clients/jovo-client-web/src/index.ts
+++ jovo-clients/jovo-client-web/src/index.ts
@@ -17,7 +17,7 @@ export type {
   CorePlatformResponseJSON as WebResponse,
 } from 'jovo-platform-web';
 // tslint:disable-next-line
-export * from 'jovo-platform-core/dist/src/Interfaces'; // ugly fix but best way to only re-export what is needed
+export * from 'jovo-platform-web/dist/src/Interfaces'; // ugly fix but best way to only re-export what is needed
 
 export * from './types';
 export * from './util';
--- jovo-platforms/jovo-platform-web/src/index.ts
+++ jovo-platforms/jovo-platform-web/src/index.ts
@@ -1,6 +1,7 @@
 import { Action, Config } from 'jovo-platform-core';
 import { WebApp } from './core/WebApp';
 
+export * from './Interfaces';
 export * from 'jovo-platform-core';
 export * from './WebPlatform';
 
