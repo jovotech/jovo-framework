d3b577babd2d584d34e2fc1548b071ecaad99564
Max Ripper <max@jovo.tech>
2020-11-25 15:18:30 +0100

    :arrow_up: Update client-version

--- jovo-clients/jovo-client-web/src/Client.ts
+++ jovo-clients/jovo-client-web/src/Client.ts
@@ -84,7 +84,7 @@ export class Client extends EventEmitter {
 
   static getDefaultConfig(): Config {
     return {
-      version: '3.2.1',
+      version: '3.2.5',
       appId: '',
       platform: '',
       device: {
