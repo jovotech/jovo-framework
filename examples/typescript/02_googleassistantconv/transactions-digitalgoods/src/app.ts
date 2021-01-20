22c13af0f334e54d733965971792ccf6c8b24a1f
Alex <a.swetlow@gmail.com>
2021-01-20 17:32:07 +0100

    🔧 Update digital goods example

--- examples/typescript/02_googleassistantconv/transactions-digitalgoods/src/app.ts
+++ examples/typescript/02_googleassistantconv/transactions-digitalgoods/src/app.ts
@@ -1,10 +1,6 @@
 import { App, Jovo } from 'jovo-framework';
 
-import {
-	GoogleAssistant,
-	PaymentParameters,
-	TransactionDecisionType,
-} from 'jovo-platform-googleassistantconv';
+import { GoogleAssistant } from 'jovo-platform-googleassistantconv';
 import { JovoDebugger } from 'jovo-plugin-debugger';
 import { FileDb } from 'jovo-db-filedb';
 
@@ -27,36 +23,25 @@ app.setHandler({
 	},
 
 	TransactionDigitalPurchaseRequirementsIntent() {
+		// check if digital purchases are available for the user
 		this.$googleAction!.$transaction!.checkDigitalPurchaseRequirements();
 		this.$googleAction!.setNextScene('TransactionDigitalPurchaseCheck');
-	},
-
-	TransactionCheckRequirementsIntent() {
-		this.$googleAction!.setNextScene('TransactionRequirementsCheck');
-	},
-
-	TransactionDeliveryAddressIntent() {
-		this.$googleAction!.$transaction!.askForDeliveryAddress(
-			'To know where to send the order'
-		);
-		this.$googleAction!.setNextScene('TransactionDeliveryAddress');
 	},
 
 	ON_TRANSACTION: {
 		async DIGITAL_PURCHASE_CHECK() {
 			if (this.$googleAction!.$transaction!.canPurchase()) {
+				const skus = await this.$googleAction!.$transaction!.getSkus(
+					['testproduct1337'],
+					'SKU_TYPE_IN_APP'
+				);
+
+				if (skus.length > 0) {
+					this.$session.$data.skuId = skus[0].skuId;
+					this.ask(`Do you want to buy ${skus[0].title}?`);
+				}
 			} else {
-				// this.tell(`You can't use digital purchasing.`);
-			}
-
-			const skus = await this.$googleAction!.$transaction!.getSkus(
-				['testproduct1337'],
-				'SKU_TYPE_IN_APP'
-			);
-
-			if (skus.length > 0) {
-				this.$session.$data.skuId = skus[0].skuId;
-				this.ask(`Do you want to buy ${skus[0].title}?`);
+				this.tell(`You can't use digital purchasing.`);
 			}
 		},
 		ON_COMPLETE_PURCHASE() {
@@ -77,7 +62,4 @@ app.setHandler({
 		this.$googleAction!.setNextScene('TransactionCompletePurchase');
 	},
 });
-function uniqueId() {
-	return Math.random().toString(36).substr(2, 9);
-}
 export { app };
