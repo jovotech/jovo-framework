31ddcde4938f8da8e33b51af84e63cc1c1aaed2f
Alex <a.swetlow@gmail.com>
2021-03-18 18:29:54 +0100

    :recycle: Export everything from core (WIP)

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,14 +1,1 @@
-export class App {
-  async bootstrap() {
-    //
-    await 1;
-  }
-
-  handle(request: any): void {
-    console.log(request);
-  }
-
-  config(config: any): this {
-    return this;
-  }
-}
+export * from '@jovotech/core';
