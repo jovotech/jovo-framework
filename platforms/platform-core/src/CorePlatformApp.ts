d716703481d0ac751eff7d63b7e79fd8c986bc03
Max Ripper <max@jovo.tech>
2021-04-23 14:03:48 +0200

    :mute: Remove test-snippet

--- platforms/platform-core/src/CorePlatformApp.ts
+++ platforms/platform-core/src/CorePlatformApp.ts
@@ -2,8 +2,4 @@ import { Jovo } from '@jovotech/core';
 import { CorePlatformResponse } from '@jovotech/output-core';
 import { CorePlatformRequest } from './CorePlatformRequest';
 
-export class CorePlatformApp extends Jovo<CorePlatformRequest, CorePlatformResponse> {
-  test() {
-    console.log('test');
-  }
-}
+export class CorePlatformApp extends Jovo<CorePlatformRequest, CorePlatformResponse> {}
