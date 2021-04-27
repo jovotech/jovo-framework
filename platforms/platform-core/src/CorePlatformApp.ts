0280d2bf5e10549fedf2f676f12d426f61744701
Max Ripper <max@jovo.tech>
2021-04-27 12:42:47 +0200

    :bug: Fix CorePlatform isNewSession-logic

--- platforms/platform-core/src/CorePlatformApp.ts
+++ platforms/platform-core/src/CorePlatformApp.ts
@@ -2,4 +2,8 @@ import { Jovo } from '@jovotech/framework';
 import { CorePlatformResponse } from '@jovotech/output-core';
 import { CorePlatformRequest } from './CorePlatformRequest';
 
-export class CorePlatformApp extends Jovo<CorePlatformRequest, CorePlatformResponse> {}
+export class CorePlatformApp extends Jovo<CorePlatformRequest, CorePlatformResponse> {
+  isNewSession(): boolean {
+    return this.$request.context?.session?.new ?? true;
+  }
+}
