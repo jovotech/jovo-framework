100d87572f6723d77f9f65fef691a8bfb69a6ea6
Ruben <email@aegerter.tech>
2021-04-28 14:51:53 +0200

    ⬆️ Update Alexa Dependency

--- examples/typescript/helloworld/src/app.dev.ts
+++ examples/typescript/helloworld/src/app.dev.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import './server.express';
-import { Alexa } from '@jovotech/platform-alexa';
+import { Alexa } from '@jovotech/platform-alexa-tmp';
 app.use(new Alexa());
 
 export { app };
