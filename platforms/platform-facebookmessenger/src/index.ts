8b21d797cd882d08c668412a90d7391ff9d96009
Max Ripper <max@jovo.tech>
2021-05-05 18:13:16 +0200

    :sparkles: :alembic: Implement Facebook Messenger-platform

---
+++ platforms/platform-facebookmessenger/src/index.ts
@@ -0,0 +1,28 @@
+import { registerPlatformSpecificJovoReference } from '@jovotech/framework';
+import { FacebookMessenger, FacebookMessengerConfig } from './FacebookMessenger';
+import { MessengerBot } from './MessengerBot';
+
+declare module '@jovotech/framework/dist/Extensible' {
+  interface ExtensiblePluginConfig {
+    FacebookMessenger?: FacebookMessengerConfig;
+  }
+
+  interface ExtensiblePlugins {
+    FacebookMessenger?: FacebookMessenger;
+  }
+}
+
+declare module '@jovotech/framework/dist/Jovo' {
+  interface Jovo {
+    $messengerBot?: MessengerBot;
+  }
+}
+registerPlatformSpecificJovoReference('$messengerBot', MessengerBot);
+
+export * from './FacebookMessenger';
+export * from './FacebookMessengerRequest';
+export * from './FacebookMessengerUser';
+export * from './MessengerBot';
+export type { FacebookMessengerResponse } from '@jovotech/output-facebookmessenger';
+export * from './interfaces';
+export * from './constants';
