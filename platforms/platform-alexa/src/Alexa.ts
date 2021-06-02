6a44247ca0994096baa1eb11119dfd15adef42f8
Ruben <email@aegerter.tech>
2021-06-02 13:04:43 +0200

    :sparkles: Support generating APL from generic output

--- platforms/platform-alexa/src/Alexa.ts
+++ platforms/platform-alexa/src/Alexa.ts
@@ -1,19 +1,24 @@
-import { ExtensibleConfig, Jovo, Platform } from '@jovotech/framework';
+import { Extensible, ExtensibleConfig, HandleRequest, Jovo, Platform } from '@jovotech/framework';
 import { AlexaOutputTemplateConverterStrategy, AlexaResponse } from '@jovotech/output-alexa';
 import { AlexaRequest } from './AlexaRequest';
 import { AlexaSkill } from './AlexaSkill';
 import { AlexaUser } from './AlexaUser';
 
-export interface AlexaConfig extends ExtensibleConfig {}
+export interface AlexaConfig extends ExtensibleConfig {
+  genericOutputToApl?: boolean;
+}
 
 export class Alexa extends Platform<AlexaRequest, AlexaResponse, AlexaSkill, AlexaConfig> {
-  outputTemplateConverterStrategy = new AlexaOutputTemplateConverterStrategy();
+  outputTemplateConverterStrategy: AlexaOutputTemplateConverterStrategy =
+    new AlexaOutputTemplateConverterStrategy();
   requestClass = AlexaRequest;
   jovoClass = AlexaSkill;
   userClass = AlexaUser;
 
-  getDefaultConfig() {
-    return {};
+  getDefaultConfig(): AlexaConfig {
+    return {
+      genericOutputToApl: true,
+    };
   }
 
   isRequestRelated(request: Record<string, any> | AlexaRequest): boolean {
@@ -22,6 +27,18 @@ export class Alexa extends Platform<AlexaRequest, AlexaResponse, AlexaSkill, Ale
 
   isResponseRelated(response: Record<string, any> | AlexaResponse): boolean {
     return response.version && response.response;
+  }
+
+  mount(parent: Extensible): void {
+    parent.middlewareCollection.use(
+      'before.request',
+      (handleRequest: HandleRequest, jovo: Jovo) => {
+        (
+          jovo.$platform.outputTemplateConverterStrategy as AlexaOutputTemplateConverterStrategy
+        ).config.genericOutputToApl =
+          jovo.$alexaSkill?.$request?.isAplSupported() && this.config.genericOutputToApl;
+      },
+    );
   }
 
   finalizeResponse(
