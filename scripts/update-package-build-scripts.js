4b06bdb5199b260c5cae239fe4bda2443175c34a
Max Ripper <max@jovo.tech>
2021-06-22 14:14:43 +0200

    :wrench: Rework build-system to use tsc -b
    - The build-mode of tsc is now used for building, this enables more efficient building.
    - Implemented script for updating all tsconfig in case changes need to be made in the future.
    - Implemented scripts for updating build-scripts of all packages. This script can also be used as a template for future scripts with similar goals.
    - Remove `resolveJsonModule` from specific tsconfigs and enable it in the root-tsconfig instead.
    
    :fire: Remove unused code
    
    :art: Clean up update-package-build-scripts
    
    :art: Improve code

---
+++ scripts/update-package-build-scripts.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { PackageGraph } = require('@lerna/package-graph');
+const { Project } = require('@lerna/project');
+const { getFilteredPackages } = require('@lerna/filter-options');
+const { join, relative } = require('path');
+
+const SCRIPTS_MAP = {
+  prebuild:
+    'tsc -b tsconfig.build.cjs.json tsconfig.build.esm5.json tsconfig.build.esm2015.json tsconfig.build.types.json --clean',
+  build:
+    'tsc -b tsconfig.build.cjs.json tsconfig.build.esm5.json tsconfig.build.esm2015.json tsconfig.build.types.json',
+  watch:
+    'tsc -b tsconfig.build.cjs.json tsconfig.build.esm5.json tsconfig.build.esm2015.json tsconfig.build.types.json --watch',
+};
+
+(async () => {
+  const cwd = join(__dirname, '..');
+  const project = new Project(cwd);
+  const packages = await project.getPackages();
+  const packageGraph = new PackageGraph(packages);
+  const filteredPackages = await getFilteredPackages(
+    packageGraph,
+    { cwd },
+    { ignore: ['@jovotech/output', '@jovotech/output-*', '@jovotech/e2e', '@jovotech/examples-*'] },
+  );
+
+  const promises = filteredPackages.map((pkg) => {
+    // SCRIPTS_MAP first in order to make prebuild the first element (small hack)
+    const mergedScripts = { ...SCRIPTS_MAP, ...pkg.scripts, ...SCRIPTS_MAP };
+    pkg.set('scripts', mergedScripts);
+    return pkg.serialize();
+  });
+  await Promise.all(promises);
+})()
+  .then(() => {
+    console.log('Success');
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error('Failure');
+    console.error((e.stdout || e.message).trim());
+    console.error(e.stack);
+    process.exit(1);
+  });
---
+++ scripts/update-tsconfig-files.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const { PackageGraph } = require('@lerna/package-graph');
+const { Project } = require('@lerna/project');
+const { getFilteredPackages } = require('@lerna/filter-options');
+const { join, relative } = require('path');
+
+const MODULE_COMPILER_OPTIONS_MAP = {
+  cjs: {
+    outDir: 'dist/cjs',
+    module: 'commonjs',
+  },
+  esm5: {
+    outDir: 'dist/esm5',
+    module: 'es2015',
+    target: 'es5',
+  },
+  esm2015: {
+    outDir: 'dist/esm2015',
+    module: 'es2015',
+  },
+  types: {
+    outDir: 'dist/types',
+    declaration: true,
+    emitDeclarationOnly: true,
+  },
+};
+
+(async () => {
+  const cwd = join(__dirname, '..');
+  const project = new Project(cwd);
+  const packages = await project.getPackages();
+  const packageGraph = new PackageGraph(packages);
+  const filteredPackages = await getFilteredPackages(
+    packageGraph,
+    { cwd },
+    { ignore: ['@jovotech/output', '@jovotech/output-*', '@jovotech/e2e', '@jovotech/examples-*'] },
+  );
+
+  const compilerOptionModuleKeys = Object.keys(MODULE_COMPILER_OPTIONS_MAP);
+
+  const promises = [];
+  for (const pkg of filteredPackages) {
+    const relativePathToRoot = relative(pkg.location, cwd);
+    const relativePathToBuildTsConfig = join(relativePathToRoot, 'tsconfig.build.json');
+    const normalizedRelativePathToBuildTsConfig = relativePathToBuildTsConfig.replace(/\\/g, '/');
+
+    const writePromises = compilerOptionModuleKeys.map((key) => {
+      const tsconfig = {
+        extends: normalizedRelativePathToBuildTsConfig,
+        compilerOptions: MODULE_COMPILER_OPTIONS_MAP[key],
+        include: ['src'],
+      };
+      const tsconfigBuffer = Buffer.from(JSON.stringify(tsconfig, undefined, 2));
+      const tsconfigPath = join(pkg.location, `tsconfig.build.${key}.json`);
+      return fs.promises.writeFile(tsconfigPath, tsconfigBuffer);
+    });
+    promises.push(...writePromises);
+  }
+  await Promise.all(promises);
+})()
+  .then(() => {
+    console.log('Success');
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error('Failure');
+    console.error((e.stdout || e.message).trim());
+    console.error(e.stack);
+    process.exit(1);
+  });
