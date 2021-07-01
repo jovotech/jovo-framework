8c2b062ec17fbba68dc92831b21ccc160d7abaed
Alex <a.swetlow@gmail.com>
2021-07-01 11:48:00 +0200

    :sparkles: Add enableFullScreen and continueTtsDuringTouch to Interactive Canvas

--- jovo-platforms/jovo-platform-googleassistant/src/response/HtmlResponse.ts
+++ jovo-platforms/jovo-platform-googleassistant/src/response/HtmlResponse.ts
@@ -2,10 +2,20 @@ export class HtmlResponse {
   url?: string;
   data?: Record<string, object>;
   suppress?: boolean;
+  enableFullScreen?: boolean;
+  continueTtsDuringTouch?: boolean;
 
-  constructor(obj: { url?: string; data?: Record<string, object>; suppress?: boolean }) {
+  constructor(obj: {
+    url?: string;
+    data?: Record<string, object>;
+    suppress?: boolean;
+    enableFullScreen?: boolean;
+    continueTtsDuringTouch?: boolean;
+  }) {
     this.url = obj.url;
     this.data = obj.data;
     this.suppress = obj.suppress;
+    this.enableFullScreen = obj.enableFullScreen;
+    this.continueTtsDuringTouch = obj.continueTtsDuringTouch;
   }
 }
