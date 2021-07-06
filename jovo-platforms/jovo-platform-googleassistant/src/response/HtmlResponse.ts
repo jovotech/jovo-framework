3ec597b451445704953a9718d112256b5cfcf507
Alex <a.swetlow@gmail.com>
2021-07-06 14:38:28 +0200

    :label: Improve types

--- jovo-platforms/jovo-platform-googleassistant/src/response/HtmlResponse.ts
+++ jovo-platforms/jovo-platform-googleassistant/src/response/HtmlResponse.ts
@@ -1,17 +1,18 @@
-export class HtmlResponse {
+export interface InteractiveCanvasResponse {
   url?: string;
   data?: Record<string, object>;
   suppress?: boolean;
   enableFullScreen?: boolean;
   continueTtsDuringTouch?: boolean;
+}
 
-  constructor(obj: {
-    url?: string;
-    data?: Record<string, object>;
-    suppress?: boolean;
-    enableFullScreen?: boolean;
-    continueTtsDuringTouch?: boolean;
-  }) {
+export class HtmlResponse implements InteractiveCanvasResponse {
+  url?: string;
+  data?: Record<string, object>;
+  suppress?: boolean;
+  enableFullScreen?: boolean;
+  continueTtsDuringTouch?: boolean;
+  constructor(obj: Partial<InteractiveCanvasResponse>) {
     this.url = obj.url;
     this.data = obj.data;
     this.suppress = obj.suppress;
