5d813b179515f0422cb4073ff8cc2b2713ddadc3
Max Ripper <max@jovo.tech>
2021-08-16 17:13:24 +0200

    :alien: Adjust Alexa to input-object

--- platforms/platform-alexa/src/AlexaPlatform.ts
+++ platforms/platform-alexa/src/AlexaPlatform.ts
@@ -4,15 +4,16 @@ import {
   EntityMap,
   ExtensibleConfig,
   HandleRequest,
+  InputType,
   Jovo,
   Platform,
 } from '@jovotech/framework';
 import { AlexaOutputTemplateConverterStrategy, AlexaResponse } from '@jovotech/output-alexa';
-import { AlexaRequest } from './AlexaRequest';
 import { Alexa } from './Alexa';
+import { AlexaDevice } from './AlexaDevice';
+import { AlexaRequest } from './AlexaRequest';
 import { AlexaUser } from './AlexaUser';
 import { SUPPORTED_APL_ARGUMENT_TYPES } from './constants';
-import { AlexaDevice } from './AlexaDevice';
 
 export interface AlexaConfig extends ExtensibleConfig {
   output: {
@@ -75,9 +76,11 @@ export class AlexaPlatform extends Platform<
     if (jovo.$alexa?.$request?.request?.type === 'Alexa.Presentation.APL.UserEvent') {
       const requestArguments = jovo.$alexa.$request.request.arguments || [];
       requestArguments.forEach((argument) => {
+        // if the user-event is an object and is of Selection or QuickReply type
         if (typeof argument === 'object' && SUPPORTED_APL_ARGUMENT_TYPES.includes(argument?.type)) {
+          jovo.$input.type = InputType.Intent;
           if (argument.intent) {
-            jovo.$nlu.intent = { name: argument.intent };
+            jovo.$input.intent = argument.intent;
           }
           if (argument.entities) {
             const entityMap: EntityMap = argument.entities.reduce(
@@ -87,7 +90,7 @@ export class AlexaPlatform extends Platform<
               },
               {},
             );
-            jovo.$nlu.entities = { ...entityMap };
+            jovo.$input.entities = { ...entityMap };
             jovo.$entities = entityMap;
           }
         }
