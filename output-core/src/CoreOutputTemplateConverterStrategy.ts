40b6c0e12ee584ad270290eefb4ada6329c5e67f
Alex <a.swetlow@gmail.com>
2021-08-18 15:39:21 +0200

    :bug: Add accidentally removed property

--- output-core/src/CoreOutputTemplateConverterStrategy.ts
+++ output-core/src/CoreOutputTemplateConverterStrategy.ts
@@ -12,6 +12,7 @@ export class CoreOutputTemplateConverterStrategy extends OutputTemplateConverter
   OutputTemplateConverterStrategyConfig
 > {
   responseClass = CoreResponse;
+  platformName = 'Core';
 
   toResponse(output: OutputTemplate | OutputTemplate[]): CoreResponse {
     output = Array.isArray(output) ? output : [output];
