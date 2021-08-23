4b63a2bffd2f3ab117f3efe0d23e9601cac36137
Max Ripper <max@jovo.tech>
2021-08-23 13:09:39 +0200

    :fire: Remove outdated comment

--- output-core/src/models/CoreResponse.ts
+++ output-core/src/models/CoreResponse.ts
@@ -9,7 +9,6 @@ import {
 } from '@jovotech/output';
 import { Context } from './Context';
 
-// TODO: Find better names for output and repromptOutputs!
 export class CoreResponse extends JovoResponse {
   @IsString()
   @IsNotEmpty()
