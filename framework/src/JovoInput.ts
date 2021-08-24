85f01d2f3fb8c82159538b8b50cd374e00408961
Max Ripper <max@jovo.tech>
2021-08-24 14:50:56 +0200

    :recycle: Refactor input and routing-logic
    - `JovoInput.nlu` is now only used by NluPlugins
    - `JovoInput.intent` is mainly used for routing
    - Logic in `RoutingExecutor` changed to use preferably use `JovoInput.type` and fallback to using `JovoInput.intent` no matter what the `type`
    - `Jovo.$input.intent` will be set by the `RouterPlugin`. This will be the mapped intent if there is any and otherwise the intent in `JovoInput.intent` or `JovoInput.nlu.intent` in that order. If neither exists, the routing will only check for type-matches and UNHANDLED-handlers that match.

--- framework/src/JovoInput.ts
+++ framework/src/JovoInput.ts
@@ -23,27 +23,18 @@ export interface AudioInput {
 export class JovoInput {
   asr?: AsrData;
   nlu?: NluData;
-
-  // InputType.Intent
   intent?: NluData['intent'];
   entities?: EntityMap;
-
-  // InputType.Text, InputType.TranscribedSpeech, InputType.Speech
   text?: string;
-
-  // InputType.Speech
   audio?: AudioInput;
 
   constructor(public type: InputTypeLike) {}
 
   getIntentName(): string | undefined {
-    function getIntentName(intent: Intent | string): string {
-      return typeof intent === 'string' ? intent : intent.name;
-    }
-    return this.type === InputType.Intent && this.intent
-      ? getIntentName(this.intent)
-      : this.nlu?.intent
-      ? getIntentName(this.nlu.intent)
-      : undefined;
+    return !this.intent
+      ? undefined
+      : typeof this.intent === 'object'
+      ? this.intent.name
+      : this.intent;
   }
 }
--- framework/src/plugins/RouterPlugin.ts
+++ framework/src/plugins/RouterPlugin.ts
@@ -1,6 +1,8 @@
 import { App } from '../App';
 import { DuplicateGlobalIntentsError } from '../errors/DuplicateGlobalIntentsError';
+import { Intent, IntentMap } from '../interfaces';
 import { Jovo } from '../Jovo';
+import { JovoInput } from '../JovoInput';
 import { HandlerMetadata } from '../metadata/HandlerMetadata';
 import { MetadataStorage } from '../metadata/MetadataStorage';
 import { Plugin, PluginConfig } from '../Plugin';
@@ -38,8 +40,19 @@ export class RouterPlugin extends Plugin<RouterPluginConfig> {
   }
 
   private setRoute = async (jovo: Jovo) => {
+    jovo.$input.intent = this.getMappedIntent(jovo.$input, jovo.$config.routing?.intentMap);
     jovo.$route = await new RoutingExecutor(jovo).execute();
   };
+
+  private getMappedIntent(input: JovoInput, intentMap?: IntentMap): Intent | string | undefined {
+    const intent = input.intent || input.nlu?.intent;
+    if (!intent) return;
+    const intentName = typeof intent === 'string' ? intent : intent.name;
+    const mappedIntentName = intentMap?.[intentName] || intentName;
+    return input.intent && typeof input.intent === 'object'
+      ? { ...input.intent, name: mappedIntentName }
+      : mappedIntentName;
+  }
 
   private checkForDuplicateGlobalHandlers(app: App): Promise<void> {
     return new Promise((resolve, reject) => {
