8cc418592144861554404ec150863cfa8d5e38fd
Max Ripper <max@jovo.tech>
2021-08-25 13:32:58 +0200

    :recycle: Correct `platformName` of `CoreOutputTemplateConverterStrategy`

--- output-core/src/CoreOutputTemplateConverterStrategy.ts
+++ output-core/src/CoreOutputTemplateConverterStrategy.ts
@@ -12,7 +12,7 @@ export class CoreOutputTemplateConverterStrategy extends OutputTemplateConverter
   OutputTemplateConverterStrategyConfig
 > {
   responseClass = CoreResponse;
-  platformName = 'Core';
+  platformName = 'core';
 
   toResponse(output: OutputTemplate | OutputTemplate[]): CoreResponse {
     output = Array.isArray(output) ? output : [output];
