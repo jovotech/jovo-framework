953b6ea417087ce6890abedc5e6befbc8f2a9eb8
Max Ripper <max@jovo.tech>
2021-08-31 13:22:29 +0200

    :sparkles: Add `key` which references the `rawValue`

--- integrations/nlu-snips/src/SnipsNlu.ts
+++ integrations/nlu-snips/src/SnipsNlu.ts
@@ -61,7 +61,11 @@ export class SnipsNlu extends NluPlugin<SnipsNluConfig> {
       }
 
       // TODO: Why is this a map when we do have to provide the name in the object itself?
-      nluData.entities[slot.slotName] = { name: slot.slotName, value: slot.value.value };
+      nluData.entities[slot.slotName] = {
+        name: slot.slotName,
+        key: slot.rawValue,
+        value: slot.value.value,
+      };
     }
 
     return nluData;
