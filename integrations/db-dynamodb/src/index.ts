3a5cce66b04bb62b3354e1be6602a596caeff22b
Max Ripper <max@jovo.tech>
2021-09-13 13:53:45 +0200

    :label: Add type-hinting

--- integrations/db-dynamodb/src/index.ts
+++ integrations/db-dynamodb/src/index.ts
@@ -1,1 +1,13 @@
+import { DynamoDb, DynamoDbConfig } from './DynamoDb';
+
+declare module '@jovotech/framework/dist/types/Extensible' {
+  interface ExtensiblePluginConfig {
+    DynamoDb?: DynamoDbConfig;
+  }
+
+  interface ExtensiblePlugins {
+    DynamoDb?: DynamoDb;
+  }
+}
+
 export { DynamoDb, DynamoDbConfig } from './DynamoDb';
