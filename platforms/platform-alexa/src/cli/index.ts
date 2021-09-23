9e362a114721f94e605e335324a09da2bce77a93
Ruben <email@aegerter.tech>
2021-09-23 16:22:26 +0200

    :mute: Remove log

--- platforms/platform-alexa/src/cli/index.ts
+++ platforms/platform-alexa/src/cli/index.ts
@@ -17,7 +17,6 @@ export class AlexaCli extends JovoCliPlugin {
   }
 
   getHooks(): typeof PluginHook[] {
-    // console.log(this.$id);
     return [BuildHook, GetHook, DeployHook, NewHook];
   }
 
