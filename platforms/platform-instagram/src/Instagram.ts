455f904c8f522253b3a2b9c0158cdbea78e5cb8b
Ruben <email@aegerter.tech>
2021-10-11 13:59:02 +0200

    :bug: Get page access token from InstagramPlatform plugin

--- platforms/platform-instagram/src/Instagram.ts
+++ platforms/platform-instagram/src/Instagram.ts
@@ -1,3 +1,7 @@
 import { FacebookMessenger } from '@jovotech/platform-facebookmessenger';
 
-export class Instagram extends FacebookMessenger {}
+export class Instagram extends FacebookMessenger {
+  get pageAccessToken(): string | undefined {
+    return this.$handleRequest.config.plugin?.InstagramPlatform?.pageAccessToken;
+  }
+}
