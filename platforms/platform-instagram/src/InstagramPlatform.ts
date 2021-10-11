5adbeca974f85261141bc3d2f5af02e442cbcc2a
Ruben <email@aegerter.tech>
2021-10-11 13:59:59 +0200

    :sparkles: Augment app.handle() inside InstagramPlatform

--- platforms/platform-instagram/src/InstagramPlatform.ts
+++ platforms/platform-instagram/src/InstagramPlatform.ts
@@ -1,3 +1,48 @@
-import { FacebookMessengerPlatform } from '@jovotech/platform-facebookmessenger';
+import { AnyObject, App, Server } from '@jovotech/framework';
+import {
+  FacebookMessengerPlatform,
+  MessengerBotEntry,
+  FacebookMessengerUser as InstagramUser,
+} from '@jovotech/platform-facebookmessenger';
+import _cloneDeep from 'lodash.clonedeep';
+import { Instagram } from './Instagram';
+import { InstagramRequest } from './InstagramRequest';
 
-export class InstagramPlatform extends FacebookMessengerPlatform {}
+export class InstagramPlatform extends FacebookMessengerPlatform {
+  readonly jovoClass = Instagram;
+  readonly requestClass = InstagramRequest;
+  readonly userClass = InstagramUser;
+
+  isRequestRelated(request: AnyObject | InstagramRequest): boolean {
+    return request.$type === 'instagram' && request.id && request.time && !!request.messaging?.[0];
+  }
+
+  augmentAppHandle(): void {
+    super.augmentAppHandle();
+    const APP_HANDLE = App.prototype.handle;
+    App.prototype.handle = async function (server: Server) {
+      const request = server.getRequestObject();
+
+      const isInstagramRequest =
+        request?.object === 'instagram' && Array.isArray(request?.entry) && request?.entry?.length;
+
+      if (isInstagramRequest) {
+        const promises = request.entry.map((entry: MessengerBotEntry) => {
+          // Set platform origin on request entry
+          entry.$type = 'instagram';
+          const serverCopy = _cloneDeep(server);
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          serverCopy.setResponse = async () => {};
+          serverCopy.getRequestObject = () => entry;
+
+          return APP_HANDLE.call(this, serverCopy);
+        });
+        await Promise.all(promises);
+        // TODO determine response content
+        return server.setResponse({});
+      } else {
+        return APP_HANDLE.call(this, server);
+      }
+    };
+  }
+}
