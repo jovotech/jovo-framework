d47c05a4695073f921eac4478f84e40b761d9d8c
Ruben <email@aegerter.tech>
2021-10-12 16:01:53 +0200

    :truck: Move NluPlugin into plugins/

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -88,7 +88,6 @@ export * from './JovoDevice';
 
 export * from './Middleware';
 export * from './MiddlewareCollection';
-export * from './NluPlugin';
 export * from './Platform';
 export * from './Plugin';
 export * from './Server';
@@ -122,13 +121,7 @@ export * from './metadata/MetadataStorage';
 export * from './metadata/MethodDecoratorMetadata';
 export * from './metadata/OutputMetadata';
 
-export * from './plugins/BasicLogging';
-export * from './plugins/DbPlugin';
-export * from './plugins/HandlerPlugin';
-export * from './plugins/OutputPlugin';
-export * from './plugins/RouteMatch';
-export * from './plugins/RouterPlugin';
-export * from './plugins/RoutingExecutor';
+export * from './plugins';
 
 export * from './interfaces';
 export * from './enums';
