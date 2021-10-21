0d73a80cfa785b78659b581a84108363daa74703
Max Ripper <max@jovo.tech>
2021-10-21 17:03:47 +0200

    :hammer: Update update peer dependencies script to update peer dependencies of output packages as well

--- scripts/update-peer-dependencies.js
+++ scripts/update-peer-dependencies.js
@@ -5,39 +5,50 @@ const { getFilteredPackages } = require('@lerna/filter-options');
 const { join, relative } = require('path');
 const log = require('npmlog');
 
+function getPackagesWithPeerDependency(packages, peerDependencyName) {
+  return packages.filter((pkg) => pkg.peerDependencies && pkg.peerDependencies[peerDependencyName]);
+}
+
+async function updatePeerDependents(cwd, packages, name) {
+  const peerDependencyPackage = packages.find((pkg) => pkg.name === name);
+
+  if (!peerDependencyPackage) {
+    throw new Error(`Can not update peerDependencies, could not find ${name}`);
+  }
+
+  log.info('peer', 'Updating %s peer-dependency to %s', name, peerDependencyPackage.version);
+
+  const filteredPackages = getPackagesWithPeerDependency(packages, name);
+  console.log(
+    name,
+    filteredPackages.map((pkg) => pkg.name),
+  );
+
+  const promises = filteredPackages.map((pkg) => {
+    pkg.peerDependencies[name] = peerDependencyPackage.version;
+    return pkg.serialize();
+  });
+  await Promise.all(promises);
+}
+
+async function updateFrameworkPeerDependents(cwd, packages) {
+  return updatePeerDependents(cwd, packages, '@jovotech/framework');
+}
+
+async function updateOutputPeerDependents(cwd, packages) {
+  return updatePeerDependents(cwd, packages, '@jovotech/output');
+}
+
 (async () => {
   const cwd = join(__dirname, '..');
   const project = new Project(cwd);
   const packages = await project.getPackages();
   const packageGraph = new PackageGraph(packages);
 
-  const frameworkPackage = packages.find((pkg) => pkg.name === '@jovotech/framework');
-
-  if (!frameworkPackage) {
-    throw new Error(`Can not update peerDependencies, could not find @jovotech/framework`);
-  }
-
-  log.info('peer', 'Updating @jovotech/framework peer-dependency to %s', frameworkPackage.version);
-
-  const filteredPackages = await getFilteredPackages(
-    packageGraph,
-    { cwd },
-    {
-      ignore: [
-        '@jovotech/framework',
-        '@jovotech/output',
-        '@jovotech/output-*',
-        '@jovotech/e2e',
-        '@jovotech/examples-*',
-      ],
-    },
-  );
-
-  const promises = filteredPackages.map((pkg) => {
-    pkg.peerDependencies['@jovotech/framework'] = frameworkPackage.version;
-    return pkg.serialize();
-  });
-  await Promise.all(promises);
+  await Promise.all([
+    updateFrameworkPeerDependents(cwd, packages, packageGraph),
+    updateOutputPeerDependents(cwd, packages, packageGraph),
+  ]);
 })()
   .then(() => {
     console.log('Success');
