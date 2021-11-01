65282e572c0ea1faa80cbae3131bfc266066d97c
Ruben <email@aegerter.tech>
2021-11-01 14:41:24 +0100

    :sparkles: Add support for platform-specific I18n translations

--- framework/src/I18Next.ts
+++ framework/src/I18Next.ts
@@ -86,16 +86,15 @@ export class I18Next extends Plugin<I18NextConfig> {
     if (options?.platform) {
       if (Array.isArray(path)) {
         for (const p of path) {
-          if (this.i18n.exists(`${options.platform}.translation.${p}`)) {
-            return this.i18n.t(`${options.platform}.translation.${p}`, options);
+          const platformPath = `${options.platform}:translation:${p}`;
+          if (this.i18n.exists(platformPath, options)) {
+            return this.i18n.t(platformPath, options);
           }
         }
       } else {
-        if (this.i18n.exists(`${options.platform}.translation.${path}`)) {
-          return this.i18n.t(
-            `${options.lng!}${options.platform}.translation.${options.platform}`,
-            options,
-          );
+        const platformPath = `${options.platform}:translation:${path}`;
+        if (this.i18n.exists(platformPath, options)) {
+          return this.i18n.t(platformPath, options);
         }
       }
     }
