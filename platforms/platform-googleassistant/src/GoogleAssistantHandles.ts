9b85a7538fa736c562dfbf54046c89bc7a1d0889
Alex <a.swetlow@gmail.com>
2021-11-02 10:55:36 +0100

    :bug: Remove global from handle

--- platforms/platform-googleassistant/src/GoogleAssistantHandles.ts
+++ platforms/platform-googleassistant/src/GoogleAssistantHandles.ts
@@ -4,7 +4,6 @@ import { GoogleAssistantRequest } from './GoogleAssistantRequest';
 export class GoogleAssistantHandles {
   static onScene(sceneName: string): HandleOptions {
     return {
-      global: true,
       types: [InputType.Intent],
       platforms: ['googleAssistant'],
       if: (jovo: Jovo) => (jovo.$request as GoogleAssistantRequest).scene?.name === sceneName,
