6c64ccf03e9384310b101d26397bdb7a72d88977
Max Ripper <max@jovo.tech>
2021-11-08 16:54:56 +0100

    :sparkles: Implement web-client plugin for Vue 2

---
+++ clients/client-web-vue2/src/index.ts
@@ -0,0 +1,37 @@
+import { Client, InitConfig } from '@jovotech/client-web';
+import { PluginObject } from 'vue';
+
+declare global {
+  interface Window {
+    JovoWebClientVue?: typeof import('.');
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $client: Client;
+  }
+}
+
+export interface JovoWebClientVueConfig {
+  url: string;
+  client?: InitConfig;
+}
+
+const plugin: PluginObject<JovoWebClientVueConfig> = {
+  install: (vue, config) => {
+    console.log('foo');
+    if (!config?.url) {
+      throw new Error(
+        `At least the 'url' option has to be set in order to use the JovoWebClientPlugin. `,
+      );
+    }
+    const client = new Client(config.url, config.client);
+    // make the client reactive
+    vue.prototype.$client = vue.observable(client);
+  },
+};
+
+export default plugin;
+
+export * from '@jovotech/client-web';
