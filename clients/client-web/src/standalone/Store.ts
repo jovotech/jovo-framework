26951c07c98d27ff959b2c93dbccf5cb0cf1fa61
Max Ripper <max@jovo.tech>
2021-11-11 19:51:11 +0100

    :alien: Adjust client to changes in Core platform

--- clients/client-web/src/standalone/Store.ts
+++ clients/client-web/src/standalone/Store.ts
@@ -1,4 +1,4 @@
-import { Session, User } from '@jovotech/platform-core';
+import { CoreRequestSession, CoreRequestUser } from '@jovotech/platform-core';
 import _defaultsDeep from 'lodash.defaultsdeep';
 import { v4 as uuidV4 } from 'uuid';
 import { DeepPartial } from '..';
@@ -7,8 +7,8 @@ import { DeepPartial } from '..';
 export type Data = Record<string, any>;
 
 export interface PersistedData {
-  user: User;
-  session?: Session;
+  user: CoreRequestUser;
+  session?: CoreRequestSession;
 }
 
 export interface StoreConfig {
@@ -29,8 +29,8 @@ export class Store {
   readonly config: StoreConfig;
 
   data: Data = {};
-  sessionData!: Session;
-  userData!: User;
+  sessionData!: CoreRequestSession;
+  userData!: CoreRequestUser;
 
   constructor(config?: DeepPartial<StoreConfig>) {
     const defaultConfig = Store.getDefaultConfig();
@@ -46,7 +46,7 @@ export class Store {
     const rawPersistedData = localStorage.getItem(this.config.storageKey) || '{}';
     const persistedData: Partial<PersistedData> = JSON.parse(rawPersistedData);
 
-    const defaultUserData: User = {
+    const defaultUserData: CoreRequestUser = {
       id: uuidV4(),
       data: {},
     };
@@ -71,7 +71,7 @@ export class Store {
     localStorage.setItem(this.config.storageKey, JSON.stringify(persistedData));
   }
 
-  private newSessionData(): Session {
+  private newSessionData(): CoreRequestSession {
     return {
       id: uuidV4(),
       data: {},
