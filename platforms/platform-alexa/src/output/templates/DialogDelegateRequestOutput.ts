563c9011706ddc4cfc16a319792f72b0f82e00c1
ruben <email@aegerter.tech>
2021-12-02 20:25:09 +0100

    :label: Improve typing to filter out invalid fields

--- platforms/platform-alexa/src/output/templates/DialogDelegateRequestOutput.ts
+++ platforms/platform-alexa/src/output/templates/DialogDelegateRequestOutput.ts
@@ -8,29 +8,68 @@ export interface UpdatedRequestSlot {
 
 export interface UpdatedRequestData {
   name: string;
-  slots: Record<string, UpdatedRequestSlot>;
+  slots?: Record<string, UpdatedRequestSlot>;
 }
 
-export interface UpdatedRequest {
-  type: 'Dialog.InputRequest' | 'IntentRequest';
-  intent?: UpdatedRequestData;
-  input?: UpdatedRequestData;
+export interface UpdatedIntentRequest {
+  type: 'IntentRequest';
+  intent: UpdatedRequestData;
 }
 
-export interface DialogDelegateRequestOutputOptions extends OutputOptions {
-  target: ConversationsTarget;
-  updatedRequest?: UpdatedRequest;
+export interface UpdatedInputRequest {
+  type: 'Dialog.InputRequest';
+  input: UpdatedRequestData;
+}
+
+export interface DialogDelegateRequestIntentOutputOptions extends OutputOptions {
+  target: 'skill';
+  updatedRequest?: UpdatedIntentRequest;
+}
+
+export interface DialogDelegateRequestInputOutputOptions extends OutputOptions {
+  target: 'AMAZON.Conversations';
+  updatedRequest?: UpdatedInputRequest;
 }
 
 @Output()
-export class DialogDelegateRequestOutput extends BaseOutput<DialogDelegateRequestOutputOptions> {
+export class DialogDelegateRequestOutput extends BaseOutput<
+  DialogDelegateRequestInputOutputOptions | DialogDelegateRequestIntentOutputOptions
+> {
   build(): OutputTemplate | OutputTemplate[] {
+    const getSlotsFromEntities = (): Record<string, UpdatedRequestSlot> => {
+      return Object.entries(this.$entities).reduce(
+        (slots: Record<string, UpdatedRequestSlot>, [entityKey, entity]) => {
+          slots[entityKey] = {
+            name: entityKey,
+            value: entity!.value,
+          };
+          return slots;
+        },
+        {},
+      );
+    };
+
+    // If dialog is delegated back to Jovo and slots are omitted, set them automatically
+    if (
+      this.options.target === 'skill' &&
+      this.options.updatedRequest?.intent &&
+      !this.options.updatedRequest.intent.slots
+    ) {
+      this.options.updatedRequest.intent.slots = getSlotsFromEntities();
+    }
+
+    if (
+      this.options.target === 'AMAZON.Conversations' &&
+      this.options.updatedRequest?.input &&
+      !this.options.updatedRequest.input.slots
+    ) {
+      this.options.updatedRequest.input.slots = getSlotsFromEntities();
+    }
+
     return {
       platforms: {
         alexa: {
           nativeResponse: {
-            version: '1.0',
-            sessionAttributes: {},
             response: {
               directives: [
                 {
