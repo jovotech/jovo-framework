ff4d020cef2b66f17496161cbdda90214a1e4c82
Alex <a.swetlow@gmail.com>
2021-12-09 15:04:48 +0100

    :recycle: Add ISP templates exports

--- platforms/platform-alexa/src/output/templates/index.ts
+++ platforms/platform-alexa/src/output/templates/index.ts
@@ -4,3 +4,6 @@ export * from './AskForPermissionOutput';
 export * from './AskForTimersPermissionOutput';
 export * from './AskForPermissionConsentCardOutput';
 export * from './AskForRemindersPermissionOutput';
+export * from './IspUpsellOutput';
+export * from './IspCancelOutput';
+export * from './IspBuyOutput';
