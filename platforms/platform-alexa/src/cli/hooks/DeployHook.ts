98ec6697cd6f2aa693823832b30108866985a530
ruben <email@aegerter.tech>
2021-12-10 09:12:45 +0100

    :bug: Fix Log property

--- platforms/platform-alexa/src/cli/hooks/DeployHook.ts
+++ platforms/platform-alexa/src/cli/hooks/DeployHook.ts
@@ -222,7 +222,7 @@ export class DeployHook extends AlexaHook<DeployPlatformEvents> {
       Log.info(
         chalk.dim(`$ ask smapi get-import-status --import-id ${this.$context.alexa.importId}`),
         {
-          indentation: 2,
+          indent: 2,
         },
       );
     }
