ad032856e83938dcf74773834d1e4e45d108a273
Alex <a.swetlow@gmail.com>
2022-01-04 17:52:27 +0100

    :sparkles: Add Alexa audio player example app

---
+++ examples/typescript/alexa/audioplayer/jovo.debugger.js
@@ -0,0 +1,16 @@
+const { DebuggerConfig } = require('@jovotech/plugin-debugger');
+
+const debuggerConfig = new DebuggerConfig({
+  locales: ['en'],
+  buttons: [
+    {
+      label: 'LAUNCH',
+      input: {
+        type: 'LAUNCH',
+      },
+    }
+    // ...
+  ],
+});
+
+module.exports = debuggerConfig;
---
+++ examples/typescript/alexa/audioplayer/src/server.lambda.ts
@@ -0,0 +1,17 @@
+import { APIGatewayEvent, Callback, Context, Lambda } from '@jovotech/server-lambda';
+import { app } from './app';
+
+/*
+|--------------------------------------------------------------------------
+| LAMBDA CONFIGURATION
+|--------------------------------------------------------------------------
+|
+| Used to run the app on AWS Lambda
+| Learn more here: www.jovo.tech/docs/server/lambda
+|
+*/
+
+export const handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  await app.initialize();
+  await app.handle(new Lambda(event, context, callback));
+};
