14ff62930851cc5ce8d2eaf5e7a1c05573ed1075
Max Ripper <max@jovo.tech>
2022-01-20 13:59:38 +0100

    :bug: Fix typing bug that would cause ComponentConfig to always return a union type with config and undefined

--- framework/src/BaseComponent.ts
+++ framework/src/BaseComponent.ts
@@ -1,12 +1,16 @@
-import { DeepPartial, UnknownObject } from '@jovotech/common';
+import { UnknownObject } from '@jovotech/common';
 import { ComponentData, ComponentOptions, JovoComponentInfo } from './index';
 import { Jovo } from './Jovo';
 import { JovoProxy } from './JovoProxy';
 import { ComponentOptionsOf } from './metadata/ComponentMetadata';
 
+// JovoComponentInfo.config is optional, therefore we need to exclude undefined
+// Otherwise the config-type will always be an union type like config | undefined
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ComponentConfig<COMPONENT extends BaseComponent = any> =
-  COMPONENT['$component']['config'];
+export type ComponentConfig<COMPONENT extends BaseComponent = any> = Exclude<
+  COMPONENT['$component']['config'],
+  undefined
+>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ComponentConstructor<COMPONENT extends BaseComponent = any> = new (
