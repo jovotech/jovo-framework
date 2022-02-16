a8c9f937090077443914d661ca7522ca939c5ad0
Jan König <jankoenig120190@googlemail.com>
2022-02-16 18:42:52 +0100

    💬 Update model

--- examples/typescript/googlebusiness/basic/src/components/GlobalComponent.ts
+++ examples/typescript/googlebusiness/basic/src/components/GlobalComponent.ts
@@ -1,9 +1,10 @@
-import { BaseComponent, Component, Global } from '@jovotech/framework';
+import { BaseComponent, Component, Global, Intents } from '@jovotech/framework';
 import { LoveHatePizzaComponent } from './LoveHatePizzaComponent';
 
 @Global()
 @Component()
 export class GlobalComponent extends BaseComponent {
+  @Intents(['HelloWorldIntent'])
   LAUNCH() {
     return this.$redirect(LoveHatePizzaComponent);
   }
