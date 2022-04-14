e8ad6c3d9165f5c200bde29951178715da9815b8
Alex <a.swetlow@gmail.com>
2022-04-14 15:19:40 +0200

    :recycle: Add additional config merge helper

--- common/src/Configurable.ts
+++ common/src/Configurable.ts
@@ -2,13 +2,17 @@ import _merge from 'lodash.merge';
 import { AnyObject, DeepPartial } from '.';
 
 export abstract class Configurable<CONFIG extends AnyObject = AnyObject> {
-  readonly config: CONFIG;
+  config: CONFIG;
   readonly initConfig?: DeepPartial<CONFIG>;
 
   constructor(config?: DeepPartial<CONFIG>) {
     this.initConfig = config;
     const defaultConfig = this.getDefaultConfig();
     this.config = config ? _merge(defaultConfig, config) : defaultConfig;
+  }
+
+  mergeConfig(config: DeepPartial<CONFIG>): void {
+    this.config = _merge(this.config, config);
   }
 
   get name(): string {
