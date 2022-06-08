64657f6da7356b8fe34cfcb59556507be8a4be33
Jan König <jankoenig120190@googlemail.com>
2022-06-08 18:05:18 +0200

    🚧  Add `setSpeech` etc. to `GoogleAssistantResponse`

--- platforms/platform-googleassistant/src/GoogleAssistantResponse.ts
+++ platforms/platform-googleassistant/src/GoogleAssistantResponse.ts
@@ -42,4 +42,34 @@ export class GoogleAssistantResponse extends JovoResponse {
   hasSessionEnded(): boolean {
     return this.scene?.next?.name === 'actions.scene.END_CONVERSATION';
   }
+
+  getSpeech(): string | undefined {
+    // We're currently only returning firstSimple
+    // tbd what should be done if lastSimple is available
+    return this.prompt?.firstSimple?.speech;
+  }
+
+  getReprompt(): string | undefined {
+    // There are 3 reprompts supported, but since currently they are the same by default,
+    // we're only returning NO_INPUT_1
+    return this.session?.params._GOOGLE_ASSISTANT_REPROMPTS_?.NO_INPUT_1;
+  }
+
+  setSpeech(speech: string): void {
+    if (this.prompt?.firstSimple) {
+      this.prompt.firstSimple.speech = speech;
+    }
+  }
+
+  setReprompt(speech: string): void {
+    // Currently, we're setting the same value for all reprompts, similar to
+    // how it's done in the GoogleAssistantOutputTemplateConverterStrategy
+    if (this.session) {
+      this.session.params._GOOGLE_ASSISTANT_REPROMPTS_ = {
+        NO_INPUT_1: speech,
+        NO_INPUT_2: speech,
+        NO_INPUT_FINAL: speech,
+      };
+    }
+  }
 }
