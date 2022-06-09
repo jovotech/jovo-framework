8ce674b02418636c797140147869f6c40dfdd84f
Jan König <jankoenig120190@googlemail.com>
2022-06-09 18:03:07 +0200

    🚧 Add `getSpeech` etc. to `CoreResponse`

--- platforms/platform-core/src/CoreResponse.ts
+++ platforms/platform-core/src/CoreResponse.ts
@@ -4,6 +4,7 @@ import {
   IsString,
   JovoResponse,
   NormalizedOutputTemplate,
+  SpeechMessage,
   Type,
   ValidateNested,
 } from '@jovotech/output';
@@ -29,5 +30,97 @@ export class CoreResponse extends JovoResponse {
 
   hasSessionEnded(): boolean {
     return this.context.session.end;
+  }
+
+  getSpeech(): string | string[] | undefined {
+    let speech: string[] = [];
+    this.output.forEach((output) => {
+      if (output.message) {
+        // The message property can either be a string or an object with a speech property.
+        if (typeof output.message === 'object' && output.message.speech) {
+          speech.push(output.message.speech);
+        }
+        if (typeof output.message === 'string') {
+          speech.push(output.message);
+        }
+      }
+    });
+
+    if (speech.length === 1) {
+      return speech[0];
+    }
+
+    if (speech.length === 0) {
+      return undefined;
+    }
+
+    return speech;
+  }
+
+  getReprompt(): string | string[] | undefined {
+    let reprompts: string[] = [];
+    this.output.forEach((output) => {
+      if (output.reprompt) {
+        // The reprompt property can either be a string or an object with a speech property.
+        if (typeof output.reprompt === 'object' && output.reprompt.speech) {
+          reprompts.push(output.reprompt.speech);
+        }
+        if (typeof output.reprompt === 'string') {
+          reprompts.push(output.reprompt);
+        }
+      }
+    });
+
+    if (reprompts.length === 1) {
+      return reprompts[0];
+    }
+
+    if (reprompts.length === 0) {
+      return undefined;
+    }
+
+    return reprompts;
+  }
+
+  replaceSpeech(speech: string | string[]): void {
+    const speechArray = Array.isArray(speech) ? speech : [speech];
+
+    for (let speechIndex = 0; speechIndex < speechArray.length; speechIndex++) {
+      for (let outputIndex = speechIndex; outputIndex < this.output.length; outputIndex++) {
+        if (this.output[outputIndex].message) {
+          if (typeof this.output[outputIndex].message === 'string') {
+            this.output[outputIndex].message = speech[speechIndex];
+            break; // continue with the next speech item (outer loop)
+          }
+
+          if (typeof this.output[outputIndex].message === 'object') {
+            // TODO: Clean this up
+            (this.output[outputIndex].message as SpeechMessage).speech = speech[speechIndex];
+            break; // continue with the next speech item (outer loop)
+          }
+        }
+      }
+    }
+  }
+
+  replaceReprompt(reprompt: string | string[]): void {
+    const repromptArray = Array.isArray(reprompt) ? reprompt : [reprompt];
+
+    for (let repromptIndex = 0; repromptIndex < repromptArray.length; repromptIndex++) {
+      for (let outputIndex = repromptIndex; outputIndex < this.output.length; outputIndex++) {
+        if (this.output[outputIndex].reprompt) {
+          if (typeof this.output[outputIndex].reprompt === 'string') {
+            this.output[outputIndex].reprompt = reprompt[repromptIndex];
+            break; // continue with the next reprompt item (outer loop)
+          }
+
+          if (typeof this.output[outputIndex].reprompt === 'object') {
+            // TODO: Clean this up
+            (this.output[outputIndex].reprompt as SpeechMessage).speech = reprompt[repromptIndex];
+            break; // continue with the next reprompt item (outer loop)
+          }
+        }
+      }
+    }
   }
 }
