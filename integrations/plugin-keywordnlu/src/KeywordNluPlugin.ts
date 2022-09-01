073388501e8871f9c3f58edfbca5c6c6b7eec1bc
jankoenig <jankoenig120190@gmail.com>
2022-09-01 15:14:11 +0200

    💥 Add locales to `keywordMap` of `KeywordNluPlugin`

--- integrations/plugin-keywordnlu/src/KeywordNluPlugin.ts
+++ integrations/plugin-keywordnlu/src/KeywordNluPlugin.ts
@@ -7,20 +7,33 @@ import {
   PluginConfig,
 } from '@jovotech/framework';
 
+
 /* Example
   {
-    start: 'StartIntent',
-    'learn more': 'LearnMoreIntent',
+    en: {
+      start: 'StartIntent',
+      'learn more': 'LearnMoreIntent',
+    },
+    de: {
+      start: 'StartIntent',
+      mehr: 'LearnMoreIntent',
+    }
   }
 */
+export interface KeywordMap {
+  [locale: string]: Record<string,string>;
+}
+
 export interface KeywordNluPluginConfig extends PluginConfig {
-  keywordMap: Record<string,string>;
+  keywordMap: KeywordMap;
+  fallbackLocale: string;
 }
 
 export class KeywordNluPlugin extends Plugin<KeywordNluPluginConfig> {
   getDefaultConfig(): KeywordNluPluginConfig {
     return {
       keywordMap: {},
+      fallbackLocale: 'en',
     };
   }
 
@@ -33,13 +46,23 @@ export class KeywordNluPlugin extends Plugin<KeywordNluPluginConfig> {
   mount(parent: Extensible): Promise<void> | void {
     parent.middlewareCollection.use('before.interpretation.nlu', (jovo: Jovo) => {
       const text = jovo.$input.getText()?.toLowerCase();
+      const locale = this.getLocale(jovo);
 
-      if (text && this.config.keywordMap.hasOwnProperty(text)) {
-          jovo.$input.intent = this.config.keywordMap[text];
+      if (text && this.config.keywordMap[locale]?.hasOwnProperty(text)) {
+          jovo.$input.intent = this.config.keywordMap[locale][text];
 
           // If a keyword matches, skip other NLU integrations for better performance
           jovo.$handleRequest.skipMiddlewares('interpretation.nlu');
       }
     });
+  }
+
+  protected getLocale(jovo: Jovo): string {
+    const locale = jovo.$request.getLocale() || this.config.fallbackLocale;
+    
+    // Only use generic locales like 'en' instead of e.g. 'en-US'
+    const genericLocale = locale.split('-')[0];
+
+    return genericLocale;
   }
 }
