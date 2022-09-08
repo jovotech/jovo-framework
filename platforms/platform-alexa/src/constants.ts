bcd2049c91a6904b7ad71edd99686769de7d0a02
jankoenig <jankoenig120190@gmail.com>
2022-09-08 12:06:16 +0200

    :bug: Only convert Alexa request type if intent is set

--- platforms/platform-alexa/src/constants.ts
+++ platforms/platform-alexa/src/constants.ts
@@ -1,3 +1,3 @@
-export const SUPPORTED_APL_ARGUMENT_TYPES = ['QuickReply', 'Selection'];
+export const SUPPORTED_APL_ARGUMENT_TYPES = ['QuickReply', 'Selection']; // @see https://www.jovo.tech/marketplace/platform-alexa/output#apl-user-events
 export const STATIC_ENTITY_MATCHES_PREFIX = 'amzn1.er-authority.echo-sdk.amzn1';
 export const DYNAMIC_ENTITY_MATCHES_PREFIX = 'amzn1.er-authority.echo-sdk.dynamic';
